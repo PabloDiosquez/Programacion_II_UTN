1e1887a058dcfc435d3a2270105cd224abf3363e
Pablo Diosquez <pablodiosquez24@gmail.com>
2022-12-23 10:02:31 -0300

    Se agrega Clase 2

---
+++ Clase_02/POO/Gato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria
+{
+    internal class Gato
+    {
+        private string nombre;
+
+        private DateTime fechaNacimiento;
+
+        private double peso;
+
+        private string tipoDePelo;
+
+        public void AsignarPeso(double peso)
+        {
+            this.peso = peso;
+        }
+    }
+}
---
+++ Clase_02/POO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
