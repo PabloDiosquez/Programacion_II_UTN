1b900dc288c5993dc3a596c16c7b8281f089510d
Pablo Diosquez <pablodiosquez24@gmail.com>
2022-12-26 22:32:59 -0300

    Actualización de la Clase 2

---
+++ Clase_02/Ejercicio_2/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Persona
+    {
+        private string nombre;
+
+        private DateTime fechaNacimiento;
+
+        private int dni;
+
+        public Persona(string nombre, DateTime fechaNacimiento, int dni)
+        {
+            AsignarNombre(nombre);
+
+            AsignarFechaNacimiento(fechaNacimiento);
+
+            AsignarDni(dni);
+        }
+
+        public string ObtenerNombre() 
+        {
+            return nombre;
+        }
+
+        public void AsignarNombre(string nombre) 
+        {
+            this.nombre = nombre;
+        }
+
+        public DateTime ObtenerFechaNacimiento() 
+        {
+            return fechaNacimiento;
+        }
+
+        public void AsignarFechaNacimiento(DateTime fecha) 
+        {
+            this.fechaNacimiento = fecha;
+        }
+
+        public int ObtenerDni() 
+        {
+            return dni;
+        }
+
+        public void AsignarDni(int dni)
+        {
+            this.dni = dni;
+        }
+    }
+}
---
+++ Clase_02/Ejercicio_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear una aplicación de consola y que contenga la clase Persona.
+
+            //Deberá tener los atributos:
+
+            //nombre
+            //fechaDeNacimiento
+            //dni
+            //Deberá tener un constructor que inicialice todos los atributos.
+
+            //Construir los siguientes métodos para la clase:
+
+            //Setter y getter para cada uno de los atributos.
+            //CalcularEdad será privado y retornará la edad de la persona calculándola a partir de la fecha de nacimiento.
+            //Mostrar retornará una cadena de texto con todos los datos de la persona, incluyendo la edad actual.
+            //EsMayorDeEdad si es mayor de edad devuelve el valor “Es mayor de edad", sino devuelve “es menor”.
+            //Instanciar 3 objetos de tipo Persona en el método Main.
+            //Mostrar quiénes son mayores de edad y quiénes no.
+
+        }
+    }
+}
