8d25d23b8367fef4ce5a6bdd67388993330a5882
Pablo Diosquez <pablodiosquez24@gmail.com>
2022-12-27 21:16:28 -0300

    Actualización de la Clase 3

---
+++ Clase_03/Logica/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Logica
+{
+    public class Usuario
+    {
+        private string nombre;
+
+        private string password;
+
+        public Usuario(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+        }
+
+        public string ObtenerNombre() 
+        {
+            return nombre;
+        }
+
+        public string ObtenerPassword() 
+        {
+            return password;
+        }
+
+        public bool EsPasswordValido(string password)
+        {
+            return this.password == password;
+        }
+    }
+}
