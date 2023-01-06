7ca032285ca658466a32df6255900486b93b4278
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-01-05 22:41:58 -0300

    Clase 6 Primer commit

---
+++ Clase_06/Clase_06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Clase_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays
+
+            int[] numeros1 = { 1, 2, 3, 4, 5, 6, 7, };
+
+            int[] numeros = new int[5];
+
+            // Cargar el Array
+
+            numeros[0] = -1;
+
+            numeros[1] = 2;
+
+            numeros[2] = -21;
+
+            numeros[3] = 0;
+
+            numeros[4] = 3;
+
+            // Recorrer el Array
+
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+
+            // forEach
+
+            foreach  (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            #endregion
+        }
+    }
+}
