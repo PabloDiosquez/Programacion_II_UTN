d5bcf7938ba67a75ef8d0e4ea31eb0c3a161a268
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-01-07 11:42:26 -0300

    Actualización Clase 6

--- Clase_06/Clase_06/Program.cs
+++ Clase_06/Clase_06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Clase_06
 {
@@ -33,12 +34,69 @@ namespace Clase_06
 
             // forEach
 
-            foreach  (int numero in numeros)
+            //foreach  (int numero in numeros)
+            //{
+            //    Console.WriteLine(numero);
+            //}
+
+            #endregion
+
+
+            #region Listas
+
+            List<string> nombres = new List<string>();
+
+            nombres.Add("Lio");
+            nombres.Add("Emiliano");
+            nombres.Add("Enzo");
+
+            // ¿Cómo recorremos la colección? 👉🏼 forEach
+
+            Console.WriteLine("### Recorriendo la colección ###");
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine($"Nombre: {nombre}");
+            }
+
+            //nombres.Remove("Emiliano");
+            nombres.RemoveAt(1); // 👉🏼 índice
+
+            Console.WriteLine(nombres[1]);
+
+            Console.WriteLine($"Cantidad de elementos: {nombres.Count}");
+
+            List<int> numeros2 = new List<int>();
+
+            numeros2.Add(1);
+            numeros2.Add(12);
+            numeros2.Add(-1);
+            numeros2.Add(0);
+            numeros2.Add(-16);
+            numeros2.Add(32);
+
+            numeros2.Sort(); // Ordena la lista de menor a mayor 
+
+            foreach (int numero in numeros2)
             {
                 Console.WriteLine(numero);
             }
 
+            numeros2.Sort(Comparacion); // Ordena la lista de mayor a menor (a Sort se le pasa una referencia al método Comparacion 👌🏼)
+
+            Console.WriteLine();
+
+            foreach (int numero in numeros2) 
+            {
+                Console.WriteLine(numero);
+            }    
+
             #endregion
+
+        }
+
+        public static int Comparacion(int a, int b) 
+        {
+            return b - a;
         }
     }
 }
