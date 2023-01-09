b004893a0005c1c10d720df09b2ee78fdd19fed3
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-01-08 21:34:08 -0300

    Actualización ejercicio 3

--- Clase_06/A_contar_palabras!__Ejercicio_3/Form1.cs
+++ Clase_06/A_contar_palabras!__Ejercicio_3/Form1.cs
@@ -19,9 +19,23 @@ namespace A_contar_palabras___Ejercicio_3
 
         private void btn_calcular_Click(object sender, EventArgs e)
         {
+            Dictionary<string, int> contadorPalabras = ObtenerContadorPalabras();
+
+            // Lista de pares 👉🏼 Clave: Valor
+
+            List<KeyValuePair<string, int>> podio = contadorPalabras.ToList();
+
+            podio.Sort(OrdenDecreciente);
+
+            MostrarPodio(podio);
+        }
+        private Dictionary<string, int> ObtenerContadorPalabras()
+        {
             string texto = rtb_contenedor.Text;
 
-            string[] palabras = texto.Split(' ');
+            char[] separador = new char[] {' ', ',', '.', ':', '\t', '\n'};
+
+            string[] palabras = texto.Split(separador, StringSplitOptions.RemoveEmptyEntries);
 
             Dictionary<string, int> dicc = new Dictionary<string, int>();
 
@@ -31,13 +45,30 @@ namespace A_contar_palabras___Ejercicio_3
                 {
                     dicc[palabra]++;
                 }
-                else 
+                else
                 {
                     dicc.Add(palabra, 1);
                 }
             }
-            
 
+            return dicc;
+        }
+
+        private int OrdenDecreciente(KeyValuePair<string, int> a, KeyValuePair<string, int> b) 
+        {
+            return b.Value - a.Value;
+        }
+
+        private void MostrarPodio(List<KeyValuePair<string, int>> podio) 
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            foreach (KeyValuePair<string, int> item in podio) 
+            {
+                sb.AppendLine($"Palabra: {item.Key} 👉🏼 Cantidad: {item.Value}");
+            }
+
+            MessageBox.Show(sb.ToString());
         }
     }
 }
