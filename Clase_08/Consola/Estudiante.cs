67be68135febb0b7f541349887071c3324301c49
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-01-19 17:09:05 -0300

    Se agrega la clase 8

---
+++ Clase_08/Consola/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    internal class Estudiante: Persona
+    {
+        private int legajo;
+
+        private List<Materia> materias;
+
+        public Estudiante(int legajo,string nombre, string apellido, int dni) : base(nombre, apellido, dni)
+        {
+            this.legajo = legajo;
+
+            materias = new List<Materia>();
+        }
+    }
+}
---
+++ Clase_08/Consola/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    internal class Persona
+    {
+        private string nombre;
+
+        private string apellido;
+
+        private int dni;
+
+        protected int edad;
+
+        public Persona(string nombre, string apellido, int dni)
+        {
+            this.nombre = nombre;
+
+            this.apellido = apellido;
+
+            this.dni = dni;
+        }
+
+        protected string Nombre { get => nombre; set => nombre = value; }
+        protected string Apellido { get => apellido; set => apellido = value; }
+        protected int Dni { get => dni; set => dni = value; }
+
+        public string MostrarDatos(Persona persona)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Nombre: {nombre}");
+            sb.AppendLine($"Apellido: {apellido}");
+            sb.AppendLine($"DNI: {dni}");
+            sb.AppendLine($"Edad: {edad}");
+
+            return sb.ToString();
+        }
+
+
+
+    }
+}
