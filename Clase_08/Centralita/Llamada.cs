740e921fe3d402ba22167e1e62d524dea74143e1
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-01-29 11:49:21 -0300

    Actualización Ejercicio Centralita

--- Clase_08/Centralita/Llamada.cs
+++ Clase_08/Centralita/Llamada.cs
@@ -6,22 +6,37 @@ using System.Threading.Tasks;
 
 namespace Centralita
 {
-
-    internal enum TipoLlamada 
-    {
-        Local,
-
-        Provincial,
-
-        Todas,
-    }
     internal class Llamada
     {
+        public enum TipoLlamada
+        {
+            Local,
+
+            Provincial,
+
+            Todas,
+        }
+
         protected float duracion;
 
         protected string nroDestino;
 
         protected string nroOrigen;
+
+        public float Duracion
+        {
+            get { return duracion; }
+        }
+
+        public string NroDestino 
+        {
+            get { return nroDestino; }
+        }
+
+        public string NroOrigen 
+        {
+            get { return nroOrigen; }
+        }
 
         public Llamada(float duaracion, string nroDestino, string nroOrigen) 
         {
@@ -46,11 +61,7 @@ namespace Centralita
 
         public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
         {
-            if (llamada1.duracion < llamada2.duracion) 
-            {
-                return 1;
-            }
-            return 2;
+            return (int) (llamada1.duracion - llamada2.duracion);
         }
     }
 }
---
+++ Clase_08/Centralita/Local.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centralita
+{
+    internal class Local : Llamada
+    {
+        protected float costo;
+
+        public Local(Llamada llamada, float costo) : this(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen, costo)
+        {
+
+        }
+
+        public Local(float duracion, string nroDestino, string nroOrigen, float costo) : base(duracion, nroDestino, nroOrigen)
+        {
+            this.costo = costo;
+        }
+        private float CostoLlamada 
+        {
+            get { return CalcularCosto(); }
+        }
+
+        private float CalcularCosto()
+        {
+            return costo * duracion;
+        }
+    }
+}
