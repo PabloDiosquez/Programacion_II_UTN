26c495a6c311ab97c2ff2568fa5bc166c46557a9
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-11 15:17:09 -0300

    Se agrega Clase 11

---
+++ Clase_11/Calculadora/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        public double Dividir(double dividendo, double divisor)
+        {
+            if (divisor != 0)
+            {
+                return dividendo / divisor; 
+            }
+            
+            return Double.MinValue;
+        }
+    }
+}
