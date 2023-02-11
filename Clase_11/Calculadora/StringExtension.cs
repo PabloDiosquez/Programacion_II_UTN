b1faaf59076bb248b17417271770202e65c51147
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-11 15:28:14 -0300

    Actualización de la Clase 11

---
+++ Clase_11/Calculadora/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class StringExtension
+    {
+        public static int ContadorDeVocales(string texto)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Clase_11/Pruebas/StringExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Pruebas
+{
+    [TestClass]
+    internal class StringExtensionTest
+    {
+        [TestMethod]
+        public void ContadorDeVocales_CuandoNoTieneVocales_DeberiaRetornarCero()
+        {
+            // Arrange 
+            int resultadoEsperado = 0;
+
+            string texto = "tyRyu nwq! ;";
+
+            // Act
+            int resultado = StringExtension.ContadorDeVocales(texto);
+
+            // Assert 
+            Assert.AreEqual(resultadoEsperado, resultado);
+
+        }
+    }
+}
