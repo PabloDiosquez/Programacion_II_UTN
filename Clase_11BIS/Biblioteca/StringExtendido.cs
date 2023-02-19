ceeb560db4faa9d9090f6411a9e8a371d11687cc
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-19 17:47:36 -0300

    Actualización Clase 11 BIS

---
+++ Clase_11BIS/Biblioteca/StringExtendido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class StringExtendido
+    {
+        public static int ContadorDeVocales(string texto)
+        {
+            char[] vocales = new char[] { 'a', 'e', 'i', 'o', 'u'};
+
+            int cont = 0;
+
+            foreach (char caracter in texto.ToLower())
+            {
+                if (vocales.Contains(caracter))
+                {
+                    cont++;
+                }
+            }
+
+            return cont;
+        }
+    }
+}
---
+++ Clase_11BIS/Pruebas/StringExtendidoPruebas.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class StringExtendidoPruebas
+    {
+        [TestMethod]
+
+        public void ContadorDeVocales_CuandoElStringNoTieneVocales_DeberiaRetornarCero()
+        {
+            // Arrange
+
+            string texto = "¡ Brrrr!    -- ;¡ Ssssh!";
+
+            int resultadoEsperado = 0;
+
+            // Act 
+
+            int resultado = StringExtendido.ContadorDeVocales(texto);
+
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void ContadorDeVocales_CuandoElStringTieneDosVocalesEnMinuscula_DeberiaRetornarDos()
+        {
+            // Arrange
+
+            string texto = "¡ Brerrr!    -- ;¡ Sasssh!";
+
+            int resultadoEsperado = 2;
+
+            // Act 
+
+            int resultado = StringExtendido.ContadorDeVocales(texto);
+
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void ContadorDeVocales_CuandoElStringTieneTresVocalesEnMayuscula_DeberiaRetornarTres()
+        {
+            // Arrange
+
+            string texto = "¡ BrErrr! U   -- ;¡ SAsssh!";
+
+            int resultadoEsperado = 3;
+
+            // Act 
+
+            int resultado = StringExtendido.ContadorDeVocales(texto);
+
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+
+    }
+}
