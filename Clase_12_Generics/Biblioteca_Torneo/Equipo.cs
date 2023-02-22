db9a2494a6d762835478696445194adf4731e985
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-22 10:47:05 -0300

    Actualización Clase 12

--- Clase_12_Generics/Biblioteca_Torneo/Equipo.cs
+++ Clase_12_Generics/Biblioteca_Torneo/Equipo.cs
@@ -12,9 +12,26 @@ namespace Biblioteca_Torneo
 
         private DateTime fechaCreacion;
 
+        public Equipo(string nombre, DateTime fechaCreacion)
+        {
+            this.nombre = nombre;
+
+            this.fechaCreacion = fechaCreacion; 
+        }
+
         public string Ficha(Equipo equipo)
         {
             return $"{equipo.nombre} fundado el {equipo.fechaCreacion}";
+        }
+
+        public static bool operator ==(Equipo equipoA, Equipo equipoB)
+        {
+            return equipoA.nombre == equipoB.nombre && equipoA.fechaCreacion == equipoB.fechaCreacion;
+        }
+
+        public static bool operator !=(Equipo equipoA, Equipo equipoB)
+        {
+            return !(equipoA == equipoB);   
         }
     }
 }
