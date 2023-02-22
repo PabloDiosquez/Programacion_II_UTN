88bbc48b02b5ca926bfe4cac8e26158e32352f00
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-22 14:30:35 -0300

    Actualización Clase 12

---
+++ Clase_12_Generics/Biblioteca_Contabilidad/Contabilidad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Contabilidad
+{
+    public class Contabilidad<T, U>
+        where T : Documento
+        where U : Documento , new()
+    {
+        private List<T> egresos;
+
+        private List<U> ingresos;
+
+        public Contabilidad()
+        {
+            egresos = new List<T>();
+
+            ingresos = new List<U>();   
+        }
+
+        public static bool operator +(Contabilidad<T, U> contabilidad, T recibo)
+        {
+            if (recibo is not null)
+            {
+                contabilidad.egresos.Add(recibo);
+                return true;    
+            }
+            return false;
+
+        }
+
+        public static bool operator +(Contabilidad<T, U> contabilidad, U recibo)
+        {
+            if (recibo is not null)
+            {
+                contabilidad.ingresos.Add(recibo);
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ Clase_12_Generics/Biblioteca_Contabilidad/Documento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Biblioteca_Contabilidad
+{
+    public class Documento
+    {
+        private int numero;
+
+        public Documento(int numero)
+        {
+            this.numero = numero;
+        }
+    }
+}
