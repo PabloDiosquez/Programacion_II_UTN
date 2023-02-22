1fca204060bf7eb7494742a7214c1191e91eb7c0
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-22 11:04:24 -0300

    Actualización Clase 12

--- Clase_12_Generics/Biblioteca_Torneo/Torneo.cs
+++ Clase_12_Generics/Biblioteca_Torneo/Torneo.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Biblioteca_Torneo
 {
-    public class Class1
+    public class Torneo<T> where T : Equipo
     {
+        private string nombre;
+
+        private List<T> equipos;
+
+        private Torneo()
+        {
+            equipos = new List<T>();
+        }
+
+        public Torneo(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public static bool operator ==(Torneo<T> torneo, Equipo equipo)
+        {
+            return torneo.equipos.Contains(equipo);
+        }
+
+        public static bool operator !=(Torneo<T> torneo, Equipo equipo)
+        {
+            return !(torneo == equipo);
+        }
+
+        
     }
 }
