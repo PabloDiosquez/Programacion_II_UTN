c8ea98c9b870dc5ffc51ea86fb0c07cb46d5e7e8
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-23 16:21:02 -0300

    Actualización Clase 13

---
+++ Clase_13_Interfaces/Biblioteca_Cartuchera/IAcciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Cartuchera
+{
+    internal interface IAcciones
+    {
+        public ConsoleColor Color { get; set; }
+
+        public float UnidadesDeEscritura { get; set; }
+
+        public EscrituraWrapper Escribir(string texto);
+
+        public bool Recargar(int unidades);
+    }
+}
---
+++ Clase_13_Interfaces/Biblioteca_Cartuchera/Lapiz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Cartuchera
+{
+    internal class Lapiz : IAcciones
+    {
+        private float tamanioMina;
+
+        public Lapiz(int unidades)
+        {
+            tamanioMina = unidades;
+        }
+
+        public ConsoleColor Color
+        {
+            get { return ConsoleColor.Gray; }
+
+            set { throw new NotImplementedException(); }
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get { return tamanioMina; }
+        }
+
+        public void Escribir(string texto)
+        {
+            foreach (char caracter in texto)
+            {
+                tamanioMina -= 0.1F;
+            }
+        }
+
+        public bool Recargar(int unidadess)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
