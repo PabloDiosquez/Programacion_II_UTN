2c9960349b5301610416fca6a77e157bd8a38919
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-23 16:06:01 -0300

    Actualización Clase 13

---
+++ Clase_13_Interfaces/Biblioteca_Cartuchera/Boligrafo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Cartuchera
+{
+    internal class Boligrafo
+    {
+        private ConsoleColor colorTinta;
+
+        private float tinta;
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+            tinta = unidades;
+            
+            colorTinta = color;
+        }
+
+        public void Escribir(string texto) 
+        {
+            foreach (char caracter in texto)
+            {
+                tinta -= 0.3F;
+            }
+        }
+    }
+}
---
+++ Clase_13_Interfaces/Biblioteca_Cartuchera/EscrituraWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Biblioteca_Cartuchera
+{
+    public class EscrituraWrapper
+    {
+        private ConsoleColor color;
+
+        private string texto;
+
+        public EscrituraWrapper(string texto, ConsoleColor color)
+        {
+            this.color = color; 
+
+            this.texto = texto; 
+        }
+
+    }
+}
