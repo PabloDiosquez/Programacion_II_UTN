b7389f4a57b4284610ea223282f29bc368363cf5
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-24 16:30:18 -0300

    Actualización Clase 13

---
+++ Clase_13_Interfaces/Biblioteca_Aduana/Paquete.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca_Aduana
+{
+    public abstract class Paquete
+    {
+        private string codigoSeguimiento;
+
+        protected decimal costoEnvio;
+
+        private string destino;
+
+        private string origen;
+
+        private double pesoKg;
+
+        protected Paquete(string codigoSeguimiento, decimal costoEnvio, string destino, string origen, double pesoKg)
+        {
+            this.codigoSeguimiento = codigoSeguimiento;
+
+            this.costoEnvio = costoEnvio;
+
+            this.destino = destino;
+
+            this.origen = origen;
+
+            this.pesoKg = pesoKg;
+        }
+
+        public abstract bool TienePrioridad { get; }
+
+        public string ObtenerInformacionDePaquete()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"### Datos del paquete ###");
+            sb.AppendLine($"Código de seguimiento: {codigoSeguimiento}");
+            sb.AppendLine($"Origen: {origen}");
+            sb.AppendLine($"Destino: {destino}");
+            sb.AppendLine($"Costo envío: {costoEnvio}");
+            sb.AppendLine($"Peso (en kg): {pesoKg}");
+
+            return sb.ToString();   
+        }
+
+    }
+}
