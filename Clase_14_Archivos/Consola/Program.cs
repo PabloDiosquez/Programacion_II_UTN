13e54bb3ef4feb188ef2683b00b19cf347f3bd5c
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-27 22:12:01 -0300

    Actualización Clase 13

---
+++ Clase_14_Archivos/Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            verificarSistemaOperativo();
+        }
+
+        static void verificarSistemaOperativo()
+        {
+            Console.WriteLine($"¿Es Windows? {OperatingSystem.IsWindows()}");
+
+            Console.WriteLine($"¿Es Linux? {OperatingSystem.IsLinux()}");
+
+            Console.WriteLine($"¿Es MacOS? {OperatingSystem.IsMacOS()}");
+
+            Console.WriteLine($"¿Es IOS? {OperatingSystem.IsIOS()}");
+
+            Console.WriteLine($"¿Es Android? {OperatingSystem.IsAndroid()}");
+        }
+    }
+}
