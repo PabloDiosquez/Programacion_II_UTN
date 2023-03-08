aed6119c97a9ca1b2e44231e198008fe47bacbaa
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-08 20:06:40 -0300

    Actualización Clase 16-17

---
+++ Clase_17/Entidades/PersonaAccesoDatos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public static class PersonaAccesoDatos
+    {
+        static string cadenaConexion;
+
+        static SqlCommand comando;
+
+        static SqlConnection conexion;
+    }
+}
