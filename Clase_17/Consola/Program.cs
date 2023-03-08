39cff8672b6ea3e9712918d9ce7cf14c0846be56
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-08 20:52:32 -0300

    Actualización Clase 17

---
+++ Clase_17/Consola/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Entidades;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> personas = PersonaAccesoDatos.Leer();
+
+            foreach (Persona persona in personas)
+            {
+                Console.WriteLine(persona.ToString());
+            }    
+
+
+        }
+    }
+}
