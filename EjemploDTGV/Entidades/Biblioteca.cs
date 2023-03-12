4cb2aceb75c6396bd4c037b92627256bd61288db
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-12 16:02:05 -0300

    Clase 18

---
+++ EjemploDTGV/Entidades/Biblioteca.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Biblioteca
+    {
+        private int codigoJuego;
+
+        private string genero;
+
+        private string juego;
+
+        private string usuario;
+
+        public Biblioteca(string usuario, string genero, string juego, int codigoJuego)
+        {
+            this.usuario = usuario;
+
+            this.genero = genero;   
+
+            this.juego = juego; 
+
+            this.codigoJuego = codigoJuego; 
+        }
+
+        public int CodigoJuego { get => codigoJuego; }
+        public string Genero { get => genero; }
+        public string Juego { get => juego; }
+        public string Usuario { get => usuario; }
+    }
+}
