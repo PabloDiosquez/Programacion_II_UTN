591ed35c9a3e8bae9ea0b8354385aaa7bc6745f7
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-12 22:02:21 -0300

    Clase 18

---
+++ EjemploDTGV/Entidades/UsuarioDAO.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class UsuarioDAO
+    {
+        static string cadenaConexion;
+
+        static SqlCommand comando;
+
+        static SqlConnection conexion;
+
+        static UsuarioDAO()
+        {
+            cadenaConexion = "";
+
+            comando = new SqlCommand();
+
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+    }
+}
