2a9128a38a2d99df000dda7454469caa2c1d5698
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-13 17:34:01 -0300

    Clase 18

--- EjemploDTGV/Entidades/JuegoDAO.cs
+++ EjemploDTGV/Entidades/JuegoDAO.cs
@@ -18,11 +18,49 @@ namespace Entidades
 
         static JuegoDAO()
         {
-            cadenaConexion = "";
+            cadenaConexion = @"Data Source=.\SQLEXPRESS;Initial Catalog=EJERCICIOS_UTN;Integrated Security=True";
 
             comando = new SqlCommand();
 
             conexion = new SqlConnection(cadenaConexion);
+
+            comando.Connection = conexion;
+
+            comando.CommandType = System.Data.CommandType.Text;
+        }
+
+        public static List<Biblioteca> Leer()
+        {
+            List<Biblioteca> lista = new List<Biblioteca>();
+
+            try
+            {
+                // Biblioteca(string usuario, string genero, string juego, int codigoJuego)
+
+                conexion.Open();
+
+                comando.CommandText = "SELECT USERNAME, GENERO, NOMBRE, CODIGO_JUEGO FROM USUARIOS AS U INNER JOIN JUEGOS AS J ON U.CODIGO_USUARIO = J.CODIGO_USUARIO;";
+
+                SqlDataReader lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    lista.Add(new Biblioteca(lector["USERNAME"].ToString(), lector["GENERO"].ToString(), lector["NOMBRE"].ToString(), Convert.ToInt32(lector["CODIGO_JUEGO"])));
+                }
+
+                lector.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conexion.Close();
+            } 
+
+            return lista;
         }
     }
 }
