f21f3a2cabe9256d7411551b2fd0952c42787d12
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-14 17:33:25 -0300

    Clase 2BIS

---
+++ Clase_02BIS/Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("°Celsius:");
+
+            string celsius = Console.ReadLine();
+
+            Console.WriteLine($"Celsius: {celsius} -- Fahrenheit: {ConvertirCelsiusAFahrenheit(celsius)}");
+        }
+
+        public static double ConvertirCelsiusAFahrenheit(string temperaturaCelsius)
+        {
+            // Convierte a double para realizar cálculos.
+
+            double celsius = Double.Parse(temperaturaCelsius);
+
+            // Covierte grados Celsius a Farenheit.
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            return fahrenheit;
+        }
+    }
+}
+
---
+++ Clase_02BIS/Entidades/Cotizador.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Cotizador
+    {
+
+    }
+}
