df7e635555cc564a1d673134a20965bef0f91a29
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-14 20:00:37 -0300

    Clase 2BIS

--- Clase_02BIS/Consola/Program.cs
+++ Clase_02BIS/Consola/Program.cs
@@ -7,11 +7,11 @@ namespace Consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("°Celsius:");
+            //Console.WriteLine("°Celsius:");
 
-            string celsius = Console.ReadLine();
+            //string celsius = Console.ReadLine();
 
-            Console.WriteLine($"Celsius: {celsius} -- Fahrenheit: {ConvertirCelsiusAFahrenheit(celsius)}");
+            //Console.WriteLine($"Celsius: {celsius} -- Fahrenheit: {ConvertirCelsiusAFahrenheit(celsius)}");
 
             /* Llamo al método estático CotizarVentaDolares de la clase Cotizador.
              Le paso entre paréntesis el único argumento que recibe, que es el monto de dólares a vender.
@@ -22,9 +22,56 @@ namespace Consola
 
             //decimal pesos = Entidades.Cotizador.CotizarVentaDolares(100); // Sin la directiva using...
 
-            decimal pesos = Cotizador.CotizarVentaDolares(100); 
+            //decimal pesos = Cotizador.CotizarVentaDolares(100);
 
-            Console.WriteLine(pesos);
+            //Console.WriteLine(pesos);
+
+            // Usando la clase Validador
+
+            // Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado anteriormente
+            // que estén dentro del rango -100 y 100.
+
+            //int minimo = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Número:");
+
+            //    int numero = int.Parse(Console.ReadLine());
+
+            //    if (Validador.Validar(numero, -100, 100))
+            //    {
+            //        Console.WriteLine($"El número {numero} está dentro del rango [-100, 100].");
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("El número NO está dentro del rango considerado.");
+            //    }
+
+            //    // Mínimo 
+
+            //    if (i == 0)
+            //    {
+            //        minimo = numero;
+            //    }
+            //    else
+            //    {
+            //        if (numero < minimo)
+            //        {
+            //            minimo = numero;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine($"Mínimo: {minimo}");
+
+
+            // Terminado el ingreso mostrar el valor mínimo ingresado, valor máximo ingresado y el promedio.
+
+
+            // Ejercicio I02 - ¿Desea continuar?
+
+            Console.WriteLine($"Suma: {SumaNumeros()}");
         }
 
         public static double ConvertirCelsiusAFahrenheit(string temperaturaCelsius)
@@ -38,6 +85,22 @@ namespace Consola
             double fahrenheit = (celsius * 9 / 5) + 32;
 
             return fahrenheit;
+        }
+
+        public static int SumaNumeros()
+        {
+            int suma = 0;
+
+            do
+            {
+                Console.WriteLine("Número:");
+
+                suma += int.Parse(Console.ReadLine());
+
+
+            } while (Validador.ValidarRespuesta());
+
+            return suma;
         }
     }
 }
---
+++ Clase_02BIS/Entidades/Validador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Validador
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            if (valor > min && valor < max)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Realizar un programa que sume números enteros hasta que el usuario lo determine por medio de un mensaje "¿Desea continuar? (S/N)".
+
+        //Crear la clase Validador y el método estático ValidarRespuesta, que validará el ingreso de respuestas.
+
+        //El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.
+
+        public static bool ValidarRespuesta()
+        {
+            Console.WriteLine("¿Desea continuar? (S/N)");
+
+            if (Console.ReadLine().ToUpper() == "S")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
