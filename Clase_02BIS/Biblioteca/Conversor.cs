50b729249f2451a0e0603c99148c8513dca8a434
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-16 15:33:53 -0300

    Actualización Clase 2BIS

---
+++ Clase_02BIS/Biblioteca/Conversor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Conversor
+    {
+        //Desarollar una clase llamada Conversor que posea dos métodos de clase(estáticos) :
+
+        //El método ConvertirDecimalABinario que convierte un número entero del sistema decimal al sistema binario.
+
+        //public string ConvertirDecimalABinario(int numeroEntero) { }
+
+        public static string ConvertirDecimalABinario(int numeroEntero)
+        {
+            string res = "";
+
+            while (numeroEntero > 0)
+            {
+                res += $"{numeroEntero % 2}";
+
+                numeroEntero = numeroEntero / 2;
+            }
+
+            return res;
+        }
+
+        //El método ConvertirBinarioADecimal que convierte un número entero del sistema binario al sistema decimal.
+
+        //public int ConvertirBinarioADecimal(int numeroEntero) { }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Clase_02BIS/Consola_Conversor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Biblioteca;
+
+namespace Consola_Conversor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Conversor.ConvertirDecimalABinario(21));
+        }
+    }
+}
