a01a1e9eb0c03ddc273ea7db6ebfe4b69035250a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-18 09:02:07 -0300

    Calculator Console App2

---
+++ Calculator/CalculatorConsoleApp2/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp2
+{
+    internal class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num2 / num1;
+                    }
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calculator/CalculatorConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CalculatorConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("🟥 Console Calculator in C# 🟥");
+
+            Console.WriteLine("----------------------------");
+
+            while (!endApp)
+            {
+                string numInput1 = string.Empty;
+
+                string numInput2 = string.Empty;
+
+                double result = double.NaN;
+
+                Console.WriteLine("Type a number, and then press Enter: ");
+
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("This is not valid input.");
+
+                    numInput1 = Console.ReadLine();
+                }
+
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine("This is not valid input.");
+
+                    numInput2 = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
