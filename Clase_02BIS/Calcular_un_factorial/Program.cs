5280836d597da001d5823329a37000776dc9b36b
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-19 10:24:19 -0300

    Actualización Clase 2BIS

---
+++ Clase_02BIS/Calcular_un_factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calcular_un_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //El factorial de un número es una operación que consiste en multiplicar un numero “n” por todos los números enteros
+
+            //positivos que estén debajo de él, por ejemplo el factorial de 3 es el resultado de multiplicar 3 por 2 por 1.
+
+            //En una aplicación de consola, desarrollar un método estático que calcule el factorial de un número dado.
+
+            Console.WriteLine("Número: ");
+
+            int numero = 0;
+
+            while (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.WriteLine("Algo malió sal...");
+            }
+
+            Console.WriteLine("\nFactorial: {0}", CalcularFactorial(numero));
+        }
+
+        public static Int32 CalcularFactorial(int numero)
+        {
+            if (numero == 0) 
+            {
+                return 1;
+            }
+
+            return CalcularFactorial(numero - 1) * numero;
+        }
+    }
+}
