98ef33815dce3250e9a5c4db7e2b0056a25abbf6
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-24 10:06:36 -0300

    Actualización Clase 3BIS

--- Clase_3BIS/Ejercicio I03 - El ejemplo universal/Estudiante.cs
+++ Clase_3BIS/Ejercicio I03 - El ejemplo universal/Estudiante.cs
@@ -22,6 +22,8 @@ namespace Ejercicio_I03___El_ejemplo_universal
             random = new Random();  
         }
 
+        public int notaRandom = random.Next(6,10);
+
         public Estudiante(string apellido, string nombre, string legajo)
         {
             this.apellido = apellido;
@@ -41,17 +43,56 @@ namespace Ejercicio_I03___El_ejemplo_universal
             this.notaSegundoParcial = notaSegundoParcial;
         }
 
-        private int CalcularPromedio()
+        private float CalcularPromedio()
         {
-            return (notaPrimerParcial + notaSegundoParcial) / 2;
+            return (notaPrimerParcial + notaSegundoParcial) / 2F;
         }
+
+        public double CalcularNotaFinal()
+        {
+            if (notaPrimerParcial >= 4 && notaSegundoParcial >= 4)
+            {
+                return notaRandom;
+            }
+
+            return -1;
+        }
+
 
         public string MostrarDatosDelAlumno()
         {
             StringBuilder sb = new StringBuilder();
 
+            sb.AppendLine("🔹 Datos del Alumno: 🔸");
+
+            sb.AppendLine($"Apellido: {apellido}");
+
+            sb.AppendLine($"Nombre: {nombre}");
+
+            sb.AppendLine($"Legajo: {legajo}");
+
+            sb.AppendLine($"Nota primer parcial: {notaPrimerParcial}");
+
+            sb.AppendLine($"Nota segundo parcial: {notaSegundoParcial}");
+
+            sb.AppendLine($"Promedio: {CalcularPromedio()}");
+
+            double notaFinal = CalcularNotaFinal();
+
+            if (notaFinal != -1)
+            {
+                sb.AppendLine($"Nota final: {notaFinal}");
+            }
+            else 
+            {
+                sb.AppendLine("Alumno desaprobado.");
+            }
 
             return sb.ToString();
         }
+
+        // TODO 1: Crear tres instancias de la clase Estudiante (tres objetos) en el método Main.
+        // TODO 2: Cargar las notas del primer y segundo parcial a todos los alumnos. Dos deberán estar aprobados y uno desaprobado.
+        // TODO 3: Mostrar los datos de todos los alumnos.
     }
 }
