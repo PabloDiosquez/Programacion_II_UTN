563127c40bb2baffb9cefb6e861756604324b7d3
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-03-24 09:46:15 -0300

    Actualización Clase 3BIS

---
+++ Clase_3BIS/Ejercicio I03 - El ejemplo universal/Estudiante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_I03___El_ejemplo_universal
+{
+    public class Estudiante
+    {
+        private string apellido;
+
+        private string legajo;
+
+        private string nombre;
+
+        private int notaPrimerParcial;
+
+        private int notaSegundoParcial;
+
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();  
+        }
+
+        public Estudiante(string apellido, string nombre, string legajo)
+        {
+            this.apellido = apellido;
+
+            this.legajo = nombre;   
+
+            this.nombre = legajo;   
+        }
+    }
+}
