1be45375e617e322003df699e5500b9e1d23862c
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-27 21:40:39 -0300

    Clase 01 - Ejercicio 2

--- Clase_01/Ejercicio_2/Program.cs
+++ Clase_01/Ejercicio_2/Program.cs
@@ -10,22 +10,49 @@ namespace Ejercicio_2
             // Se debe validar que el número sea mayor que cero, caso contrario,
             // mostrar el mensaje: "ERROR. ¡Reingresar número!".
 
-            while (true) 
+            mostrarNumeroAlCuadYAlCubo(validarNumero());
+           
+        }
+
+        // Describe un número entero que cumple con las condiciones pedidas en el ejercicio.
+        private static int validarNumero()
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número natural:");
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
             {
-                Console.WriteLine("Ingrese un número natural:");
-                    
-                if (int.TryParse(Console.ReadLine(), out int numero) && numero > 0) 
-                {
-                    int cuadrado = numero * numero;
-                    int cubo = cuadrado * numero; 
-
-                    Console.WriteLine($"{numero} elevado al cuadrado: {cuadrado}");
-                    Console.WriteLine($"{numero} elevado al cubo: {cubo}");
-                    break;
-                }
-
                 Console.WriteLine("ERROR. ¡Reingresar número!");
             }
+
+            return numero;
+        }
+
+        // Muestra los resultados en consola.
+        private static void mostrarNumeroAlCuadYAlCubo(int numero)
+        {
+            Console.WriteLine($"{numero} elevado al cuadrado: {cuadradoDe_(numero)}");
+            Console.WriteLine($"{numero} elevado al cubo: {cuboDe_(numero)}");
+        }
+
+        /// <summary>
+        /// Describe el cuadrado del número dado.
+        /// </summary>
+        /// <param name="numero">Número</param>
+        /// <returns>Número</returns>
+        private static int cuadradoDe_(int numero) 
+        {
+            return numero * numero;
+        }
+
+        /// <summary>
+        /// Describe el cubo del número dado.
+        /// </summary>
+        /// <param name="numero">Número</param>
+        /// <returns>Número</returns>
+        private static int cuboDe_(int numero)
+        {
+            return numero * cuadradoDe_(numero);
         }
     }
 }
