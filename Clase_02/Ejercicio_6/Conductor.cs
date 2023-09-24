eecba79a6085983e4d7be06a3b7f0927b6f02e4b
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-24 09:54:12 -0300

    Se agregan modificaciones de documentación y código

---
+++ Clase_02/Ejercicio_6/Conductor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    internal class Conductor
+    {
+        // Atributos 
+
+        private int legajo;
+
+        private string apellido;
+
+        private string nombre;
+
+        private int[] diasSemana;
+
+        // Constructor 👷🏼‍
+
+        public Conductor(int legajo, string apellido, string nombre)
+        {
+            this.legajo   = legajo;
+
+            this.apellido = apellido;   
+
+            this.nombre   = nombre;
+
+            diasSemana    = new int[7];
+        }
+
+        // Métodos de instancia 
+
+        public int GetLegajo()
+        {
+            return legajo;
+        }
+
+        public string GetApellido() 
+        {
+            return apellido;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public void CargarKilometrosRecorridosPorDia() 
+        {
+            for (int i = 0; i < diasSemana.Length; i++) 
+            {
+                Console.WriteLine($"Día {i+1}: ");
+
+                diasSemana[i] = int.Parse(Console.ReadLine());
+            } 
+        }
+
+        public int GetKilometrosRecorridosEnDia(string diaSemana)
+        {
+            diaSemana = diaSemana.ToLower();
+
+            int kilometrosRecorridos;
+
+            switch (diaSemana)
+            {
+                case "lunes":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "martes":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "miercoles":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "jueves":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "viernes":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "sabado":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                case "domingo":
+                    kilometrosRecorridos = diasSemana[0];
+                    break;
+                default:
+                    Console.WriteLine("Día inválido");
+                    kilometrosRecorridos = -1;
+                    break;
+            }
+
+            return kilometrosRecorridos;
+        }
+
+        public void MostrarInfoConductor()
+        {
+            Console.WriteLine($"Legajo: {GetLegajo()}");
+
+            Console.WriteLine($"Apellido: {GetApellido()}");
+
+            for (int i = 0; i < diasSemana.Length; i++)
+            {
+                Console.WriteLine($"Día {i+1}: {diasSemana[i]}");
+            }
+        }
+
+    }
+}
---
+++ Clase_02/Ejercicio_6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //De una empresa de transporte de cargas se quiere guardar el nombre de los conductores y los kilómetros que conducen
+            //cada día de la semana.
+            //Para guardar esta información, la empresa de transporte tendrá un array de conductores.
+            //De cada conductor se tendrá la siguiente información:
+
+            //Nombre del conductor.
+            //Kilometros recorridos por cada día de la semana. Por ejemplo:
+            //Día 1: 200
+            //Día 2: 599
+            //Día 3: 899
+            //Día 4: 0(tiene franco)
+            //Día 5: 256
+            //Día 6: 0
+            //Día 7: 0
+            //Consigna
+            //Desarrollar las clases y métodos necesarios.Toda la lógica de negocio deberá estar encapsulada en clases y dentro de
+            //un proyecto de tipo biblioteca de clases.
+            //Crear un proyecto de consola y en el método Main cargar 3 conductores a la empresa de transportes.
+            //Luego mostrar:
+            //El conductor que hizo más km en esa semana.
+            //El conductor que hizo más km el día 3.
+            //El conductor que hizo más km el día 5.
+
+            Conductor conductor = new Conductor(123, "Messi", "Lio");
+
+            conductor.CargarKilometrosRecorridosPorDia();
+
+            conductor.MostrarInfoConductor();
+
+        }
+    }
+}
