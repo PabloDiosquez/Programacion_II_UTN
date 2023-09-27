89b21638903142f0e2896954401a624158b9a8d5
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-27 10:24:04 -0300

    Se agrega 'La Estantería'

--- Clase_03/3_LaEstanteria/Estante.cs
+++ Clase_03/3_LaEstanteria/Estante.cs
@@ -6,33 +6,61 @@ using System.Threading.Tasks;
 
 namespace _3_LaEstanteria
 {
+    /// <summary>
+    /// Representa un estante que almacena productos en una ubicación específica.
+    /// </summary>
     public class Estante
     {
         // Atributos 
 
+        /// <summary>
+        /// Almacena la ubicación física del estante en un lugar específico.
+        /// </summary>
         private int ubicacionEstante;
 
+        /// <summary>
+        /// Almacena una matriz de objetos de tipo Producto que representan los productos almacenados en el estante.
+        /// </summary>
         private Producto[] productos;
 
         // Constructor 
 
+        /// <summary>
+        /// Constructor privado que inicializa un estante con una capacidad especificada.
+        /// </summary>
+        /// <param name="capacidad">La cantidad máxima de productos que puede contener el estante.</param>
         private Estante(int capacidad)
         {
             productos = new Producto[capacidad];
         }
 
+        /// <summary>
+        /// Constructor público que inicializa un estante con una capacidad y ubicación específicas.
+        /// </summary>
+        /// <param name="capacidad">La cantidad máxima de productos que puede contener el estante.</param>
+        /// <param name="ubicacion">La ubicación física del estante en un lugar específico.</param>
         public Estante(int capacidad, int ubicacion) : this(capacidad)
         {
             this.ubicacionEstante = ubicacion;
         }
 
         // Métodos de instancia 
+
+        /// <summary>
+        /// Obtiene la matriz de productos almacenados en el estante.
+        /// </summary>
+        /// <returns>Una matriz de objetos de tipo Producto.</returns>
         public Producto[] GetProductos()
         {
             return productos;
         }
 
-        public static string MostrarEstante(Estante estante) 
+        /// <summary>
+        /// Muestra una representación detallada de los productos almacenados en el estante en forma de una cadena de texto.
+        /// </summary>
+        /// <param name="estante">El estante que se desea mostrar.</param>
+        /// <returns>Una cadena de texto que describe los productos en el estante.</returns>
+        public static string MostrarEstante(Estante estante)
         {
             StringBuilder sb = new StringBuilder();
 
@@ -40,20 +68,85 @@ namespace _3_LaEstanteria
             {
                 sb.AppendLine(Producto.MostrarProducto(producto));
             }
-            return sb.ToString();   
+            return sb.ToString();
         }
 
         // Sobrecarga de operadores 
 
+        /// <summary>
+        /// Comprueba si un producto está contenido en el estante.
+        /// </summary>
+        /// <param name="estante">El estante en el que se busca el producto.</param>
+        /// <param name="producto">El producto que se busca en el estante.</param>
+        /// <returns>True si el producto está en el estante, False en caso contrario.</returns>
         public static bool operator ==(Estante estante, Producto producto)
         {
             return estante.GetProductos().Contains(producto);
         }
 
+        /// <summary>
+        /// Comprueba si un producto no está contenido en el estante.
+        /// </summary>
+        /// <param name="estante">El estante en el que se busca el producto.</param>
+        /// <param name="producto">El producto que se busca en el estante.</param>
+        /// <returns>True si el producto no está en el estante, False en caso contrario.</returns>
         public static bool operator !=(Estante estante, Producto producto)
         {
             return !(estante == producto);
         }
 
+        /// <summary>
+        /// Agrega un producto al estante si hay espacio disponible.
+        /// </summary>
+        /// <param name="estante">El estante al que se intenta agregar el producto.</param>
+        /// <param name="producto">El producto que se intenta agregar.</param>
+        /// <returns>True si el producto se agregó correctamente, False si no hay espacio disponible.</returns>
+        public static bool operator +(Estante estante, Producto producto)
+        {
+            int indiceVacio = -1;
+
+            for (int i = 0; i < estante.GetProductos().Length; i++)
+            {
+                if (estante.GetProductos()[i] is null)
+                {
+                    indiceVacio = i;
+                    break;
+                }
+            }
+
+            if (indiceVacio != -1)
+            {
+                estante.GetProductos()[indiceVacio] = producto;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Elimina un producto del estante si está presente.
+        /// </summary>
+        /// <param name="estante">El estante del que se intenta eliminar el producto.</param>
+        /// <param name="producto">El producto que se intenta eliminar.</param>
+        /// <returns>True si el producto se eliminó correctamente, False si el producto no está presente en el estante.</returns>
+        public static bool operator -(Estante estante, Producto producto)
+        {
+            int indiceBuscado = -1;
+
+            for (int i = 0; i < estante.GetProductos().Length; i++)
+            {
+                if (estante.GetProductos()[i].ToString() == producto.ToString())
+                {
+                    indiceBuscado = i;
+                    break;
+                }
+            }
+
+            if (indiceBuscado != -1)
+            {
+                estante.GetProductos()[indiceBuscado] = null;
+                return true;
+            }
+            return false;
+        }
     }
 }
