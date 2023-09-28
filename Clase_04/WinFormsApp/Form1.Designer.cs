82935c3e8f47c3b4ad870e9da2c1fbdd29fcfcbc
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-27 21:53:32 -0300

    Se agrega 'WinForms App'

---
+++ Clase_04/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_mensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_mensaje
+            // 
+            this.btn_mensaje.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_mensaje.Location = new System.Drawing.Point(57, 119);
+            this.btn_mensaje.Name = "btn_mensaje";
+            this.btn_mensaje.Size = new System.Drawing.Size(75, 23);
+            this.btn_mensaje.TabIndex = 0;
+            this.btn_mensaje.Text = "Mensaje";
+            this.btn_mensaje.UseVisualStyleBackColor = false;
+            this.btn_mensaje.Click += new System.EventHandler(this.btn_mensaje_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 171);
+            this.Controls.Add(this.btn_mensaje);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_mensaje;
+    }
+}
---
+++ Clase_04/WinFormsApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_mensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Este es un mensaje envíado a través del espacio-tiempo 🚀");
+        }
+    }
+}
