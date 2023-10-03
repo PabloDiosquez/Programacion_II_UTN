e498a35f155e7db9427462c37acefb2d335efa62
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-03 15:36:39 -0300

    Se agrega Ejercicio_I02

---
+++ Clase_04/Ejercicio C03/Conversor.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Ejercicio_C03
+{
+    partial class Conversor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_decimal_binario = new System.Windows.Forms.Label();
+            this.lb_binario_decimal = new System.Windows.Forms.Label();
+            this.tbx_decimal_a_binario = new System.Windows.Forms.TextBox();
+            this.tbx_binario_a_decimal = new System.Windows.Forms.TextBox();
+            this.tbx_resultado_decimal_a_binario = new System.Windows.Forms.TextBox();
+            this.txb_resultado_binario_a_decimal = new System.Windows.Forms.TextBox();
+            this.btn_decimal_a_binario = new System.Windows.Forms.Button();
+            this.btn_binario_a_decimal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_decimal_binario
+            // 
+            this.lb_decimal_binario.AutoSize = true;
+            this.lb_decimal_binario.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_decimal_binario.Location = new System.Drawing.Point(12, 20);
+            this.lb_decimal_binario.Name = "lb_decimal_binario";
+            this.lb_decimal_binario.Size = new System.Drawing.Size(131, 19);
+            this.lb_decimal_binario.TabIndex = 0;
+            this.lb_decimal_binario.Text = "De Decimal a Binario";
+            // 
+            // lb_binario_decimal
+            // 
+            this.lb_binario_decimal.AutoSize = true;
+            this.lb_binario_decimal.Location = new System.Drawing.Point(10, 67);
+            this.lb_binario_decimal.Name = "lb_binario_decimal";
+            this.lb_binario_decimal.Size = new System.Drawing.Size(131, 19);
+            this.lb_binario_decimal.TabIndex = 1;
+            this.lb_binario_decimal.Text = "De Binario a Decimal";
+            // 
+            // tbx_decimal_a_binario
+            // 
+            this.tbx_decimal_a_binario.Location = new System.Drawing.Point(149, 20);
+            this.tbx_decimal_a_binario.Name = "tbx_decimal_a_binario";
+            this.tbx_decimal_a_binario.Size = new System.Drawing.Size(122, 24);
+            this.tbx_decimal_a_binario.TabIndex = 2;
+            // 
+            // tbx_binario_a_decimal
+            // 
+            this.tbx_binario_a_decimal.Location = new System.Drawing.Point(149, 65);
+            this.tbx_binario_a_decimal.Name = "tbx_binario_a_decimal";
+            this.tbx_binario_a_decimal.Size = new System.Drawing.Size(122, 24);
+            this.tbx_binario_a_decimal.TabIndex = 3;
+            // 
+            // tbx_resultado_decimal_a_binario
+            // 
+            this.tbx_resultado_decimal_a_binario.Location = new System.Drawing.Point(380, 19);
+            this.tbx_resultado_decimal_a_binario.Name = "tbx_resultado_decimal_a_binario";
+            this.tbx_resultado_decimal_a_binario.Size = new System.Drawing.Size(122, 24);
+            this.tbx_resultado_decimal_a_binario.TabIndex = 4;
+            // 
+            // txb_resultado_binario_a_decimal
+            // 
+            this.txb_resultado_binario_a_decimal.Location = new System.Drawing.Point(380, 64);
+            this.txb_resultado_binario_a_decimal.Name = "txb_resultado_binario_a_decimal";
+            this.txb_resultado_binario_a_decimal.Size = new System.Drawing.Size(122, 24);
+            this.txb_resultado_binario_a_decimal.TabIndex = 5;
+            // 
+            // btn_decimal_a_binario
+            // 
+            this.btn_decimal_a_binario.Location = new System.Drawing.Point(285, 19);
+            this.btn_decimal_a_binario.Name = "btn_decimal_a_binario";
+            this.btn_decimal_a_binario.Size = new System.Drawing.Size(75, 23);
+            this.btn_decimal_a_binario.TabIndex = 6;
+            this.btn_decimal_a_binario.Text = "=>";
+            this.btn_decimal_a_binario.UseVisualStyleBackColor = true;
+            // 
+            // btn_binario_a_decimal
+            // 
+            this.btn_binario_a_decimal.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_binario_a_decimal.Location = new System.Drawing.Point(285, 64);
+            this.btn_binario_a_decimal.Name = "btn_binario_a_decimal";
+            this.btn_binario_a_decimal.Size = new System.Drawing.Size(75, 25);
+            this.btn_binario_a_decimal.TabIndex = 7;
+            this.btn_binario_a_decimal.Text = "=>";
+            this.btn_binario_a_decimal.UseVisualStyleBackColor = true;
+            // 
+            // Conversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 116);
+            this.Controls.Add(this.btn_binario_a_decimal);
+            this.Controls.Add(this.btn_decimal_a_binario);
+            this.Controls.Add(this.txb_resultado_binario_a_decimal);
+            this.Controls.Add(this.tbx_resultado_decimal_a_binario);
+            this.Controls.Add(this.tbx_binario_a_decimal);
+            this.Controls.Add(this.tbx_decimal_a_binario);
+            this.Controls.Add(this.lb_binario_decimal);
+            this.Controls.Add(this.lb_decimal_binario);
+            this.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Conversor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor Numérico ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_decimal_binario;
+        private System.Windows.Forms.Label lb_binario_decimal;
+        private System.Windows.Forms.TextBox tbx_decimal_a_binario;
+        private System.Windows.Forms.TextBox tbx_binario_a_decimal;
+        private System.Windows.Forms.TextBox tbx_resultado_decimal_a_binario;
+        private System.Windows.Forms.TextBox txb_resultado_binario_a_decimal;
+        private System.Windows.Forms.Button btn_decimal_a_binario;
+        private System.Windows.Forms.Button btn_binario_a_decimal;
+    }
+}
---
+++ Clase_04/Ejercicio C03/Helper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C03
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// El propósito del método DeDecimalABinario es tomar un número decimal como entrada y 
+        /// convertirlo en su equivalente binario. El método realiza esta conversión mediante el
+        /// cálculo de los dígitos binarios del número decimal y construyendo una cadena que 
+        /// representa la representación binaria del número.
+        /// </summary>
+        /// <param name="numeroDecimal">Este es el número decimal que se desea convertir en binario. 
+        /// Es el único parámetro de entrada del método.</param>
+        /// <returns>El método devuelve una cadena de caracteres (string) que resulta la representación
+        /// binaria del número decimal proporcionado como entrada. Esta cadena contiene los dígitos binarios
+        /// en orden correcto.</returns>
+        public static string DeDecimalABinario(int numeroDecimal)
+        {
+            // Caso especial 
+            if (numeroDecimal == 0) 
+            {
+                return "0";
+            }
+
+            // Inicializar una cadena de caracteres para almacenar el resultado binario.
+            string resBinario = string.Empty;
+
+            // Mientras el número decimal sea mayor que 0, continuamos dividiendo y obteniendo los dígitos binarios.
+            while (numeroDecimal > 0)
+            {
+                // Calculamos el residuo de la división del número decimal por 2.
+                string digito = Convert.ToString(numeroDecimal % 2);
+
+                // Agregamos el dígito obtenido al principio de la cadena de resultado.
+                resBinario = digito + resBinario;
+
+                // Dividimos el número decimal por 2 para continuar el proceso.
+                numeroDecimal = numeroDecimal / 2;
+            }
+
+            // Una vez que el número decimal se ha convertido completamente en binario, retornamos la cadena resultante.
+            return resBinario;
+        }
+
+        /// <summary>
+        /// El propósito del método DeBinarioADecimal es tomar una cadena de caracteres que representa un número
+        /// binario y convertirla en su equivalente decimal. El método realiza esta conversión calculando el
+        /// valor decimal del número binario y lo devuelve como un entero.
+        /// </summary>
+        /// <param name="numeroBinario">Esta es la cadena de caracteres que representa el número binario que se desea
+        /// convertir en decimal.</param>
+        /// <returns>un número entero (int) que representa el valor decimal equivalente al número binario proporcionado
+        /// como entrada.</returns>
+        public static int DeBinarioADecimal(string numeroBinario)
+        {
+            int orden = numeroBinario.Length;
+
+            int numeroDecimal = 0;
+
+            for (int index = 0; index < orden; index++)
+            {
+                numeroBinario += Convert.ToInt32(numeroBinario[index]) * Math.Pow(2, orden - index - 1);
+            }
+
+            return numeroDecimal;
+        }
+    }
+}
