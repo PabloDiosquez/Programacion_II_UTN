71701cb3a5e13dc9cf102200973485328148159a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-05 19:02:21 -0300

    Se agrega Ejercicio3BIS

--- Clase_06/Ejercicio_3BIS/Form1.cs
+++ Clase_06/Ejercicio_3BIS/Form1.cs
@@ -18,13 +18,40 @@ namespace Ejercicio_3BIS
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Maneja el evento Click del botón "Calcular". Este método procesa el texto ingresado en el cuadro de texto
+        /// para contar las palabras y muestra un cuadro de diálogo con el recuento de palabras ordenado.
+        /// </summary>
+        /// <param name="sender">El objeto que desencadenó el evento (el botón).</param>
+        /// <param name="e">Los argumentos del evento.</param>
         private void btn_calcular_Click(object sender, EventArgs e)
         {
+            // Obtiene el texto ingresado en el cuadro de texto.
             string texto = rtb_contador.Text;
 
-            MessageBox.Show(MostrarDatos(ContadorDePalabras(texto)));
+            // Verifica si el texto no está vacío.
+            if (!string.IsNullOrEmpty(texto))
+            {
+                // Llama a la función ContadorDePalabras para contar las palabras y las ordena en orden descendente por cantidad.
+                var listaDeParesClaveValorOrdenada = ContadorDePalabras(texto).OrderByDescending(x => x.Value);
 
-            this.Close();
+                // Inicializa una cadena para almacenar las palabras y sus conteos.
+                string palabras = string.Empty;
+
+                // Recorre la lista de pares clave-valor y construye la cadena de palabras.
+                foreach (var parClaveValor in listaDeParesClaveValorOrdenada)
+                {
+                    palabras += $"{parClaveValor.Key}: {parClaveValor.Value}\n";
+                }
+
+                // Muestra un cuadro de diálogo con las palabras y sus conteos ordenados.
+                MessageBox.Show(palabras, "Diccionario de palabras", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Muestra un cuadro de diálogo de error si el cuadro de texto está vacío.
+                MessageBox.Show("Por favor, ingrese texto en la caja de texto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
