943bcac48b12f1da9261d592851a82681e62ec6e
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-08 17:33:38 -0300

    Ejercicio I01

---
+++ Clase_7/Biblioteca_EjercicioI01/Negocio.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_EjercicioI01
+{
+    /// <summary>
+    /// Clase que representa un negocio con una cola de clientes y una caja de atención.
+    /// </summary>
+    public class Negocio
+    {
+        // Atributos 
+
+        private string nombre;
+        private PuestoDeAtencion caja;
+        private Queue<Cliente> clientes;
+
+        // Constructor 
+
+        /// <summary>
+        /// Constructor privado para crear una instancia de la clase Negocio.
+        /// </summary>
+        private Negocio()
+        {
+            clientes = new Queue<Cliente>();
+            caja = new PuestoDeAtencion(Puesto.Caja1);
+        }
+
+        /// <summary>
+        /// Constructor que crea una instancia de la clase Negocio con un nombre especificado.
+        /// </summary>
+        /// <param name="nombre">El nombre del negocio.</param>
+        public Negocio(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        // Propiedades
+
+        /// <summary>
+        /// Obtiene el cliente que está siendo atendido actualmente y lo elimina de la cola de clientes.
+        /// </summary>
+        public Cliente Cliente
+        {
+            get { return this.clientes.Dequeue(); }
+
+            set
+            {
+                if (this != value) this.clientes.Enqueue(value);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene la cantidad de clientes pendientes en la cola.
+        /// </summary>
+        public int ClientesPendientes
+        {
+            get { return clientes.Count; }
+        }
+
+        // Sobrecarga de operadores 
+
+        /// <summary>
+        /// Comprueba si un cliente ya se encuentra en la cola del negocio.
+        /// </summary>
+        /// <param name="negocio">El negocio en el que se realiza la verificación.</param>
+        /// <param name="clienteNuevo">El cliente que se desea verificar.</param>
+        /// <returns>True si el cliente no está en la cola, false en caso contrario.</returns>
+        public static bool operator ==(Negocio negocio, Cliente clienteNuevo)
+        {
+            foreach (Cliente cliente in negocio.clientes)
+            {
+                if (cliente == clienteNuevo) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Comprueba si un cliente ya se encuentra en la cola del negocio.
+        /// </summary>
+        /// <param name="negocio">El negocio en el que se realiza la verificación.</param>
+        /// <param name="cliente">El cliente que se desea verificar.</param>
+        /// <returns>True si el cliente está en la cola, false en caso contrario.</returns>
+        public static bool operator !=(Negocio negocio, Cliente cliente)
+        {
+            return !(negocio == cliente);
+        }
+
+        /// <summary>
+        /// Agrega un cliente a la cola del negocio si no está presente en ella.
+        /// </summary>
+        /// <param name="negocio">El negocio al que se desea agregar el cliente.</param>
+        /// <param name="cliente">El cliente que se desea agregar.</param>
+        /// <returns>True si el cliente se agregó a la cola, false si ya estaba presente.</returns>
+        public static bool operator +(Negocio negocio, Cliente cliente)
+        {
+            if (negocio == cliente) return false;
+
+            negocio.clientes.Enqueue(cliente);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Atiende al próximo cliente en la cola utilizando la caja de atención del negocio.
+        /// </summary>
+        /// <param name="negocio">El negocio que realiza la atención.</param>
+        /// <returns>True si se atendió a un cliente, false si no hay clientes en espera.</returns>
+        public static bool operator ~(Negocio negocio)
+        {
+            if (negocio.clientes.Count > 0)
+            {
+                Cliente cliente = negocio.clientes.Dequeue();
+                negocio.caja.AtenderCliente(cliente);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Clase_7/Biblioteca_EjercicioI01/PuestoDeAtencion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biblioteca_EjercicioI01
+{
+    /// <summary>
+    /// Enumerador que define los posibles puestos de atención.
+    /// </summary>
+    public enum Puesto
+    {
+        Caja1,
+        Caja2,
+    }
+
+    /// <summary>
+    /// Clase que representa un puesto de atención en un establecimiento.
+    /// </summary>
+    public class PuestoDeAtencion
+    {
+        // Atributos
+
+        private static int numeroActual;
+        private Puesto puesto;
+
+        // Constructores
+
+        /// <summary>
+        /// Constructor estático que inicializa el número actual en cero.
+        /// </summary>
+        static PuestoDeAtencion()
+        {
+            numeroActual = 0;
+        }
+
+        /// <summary>
+        /// Constructor que crea un puesto de atención con el tipo de puesto especificado.
+        /// </summary>
+        /// <param name="puesto">El tipo de puesto de atención (Caja1 o Caja2).</param>
+        public PuestoDeAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+
+        // Métodos de instancia 
+
+        /// <summary>
+        /// Atiende a un cliente en el puesto de atención.
+        /// </summary>
+        /// <param name="cliente">El cliente que se va a atender.</param>
+        /// <returns>Devuelve true si la atención fue exitosa, false en caso contrario.</returns>
+        public bool AtenderCliente(Cliente cliente)
+        {
+            Console.WriteLine("Inicia la atención al cliente.");
+            Thread.Sleep(4000); // Simula un proceso de atención que dura 4 segundos.
+            Console.WriteLine("Cliente atendido.");
+            return true;
+        }
+
+        // Propiedades 
+
+        /// <summary>
+        /// Obtiene el número actual de atención y lo incrementa en uno.
+        /// </summary>
+        /// <returns>El número de atención actual.</returns>
+        public static int NumeroActual()
+        {
+            numeroActual++;
+            return numeroActual;
+        }
+    }
+}
