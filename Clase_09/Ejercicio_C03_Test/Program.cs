a43f72e8fb1cc01562d27a76a993c7177b9b7c8e
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-15 10:32:22 -0300

    Se agrega una pequeña prueba en el Program

---
+++ Clase_09/Ejercicio_C03_Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Ejercicio_C03_Biblioteca;
+
+namespace Ejercicio_C03_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mi central
+            Centralita c = new Centralita("Fede Center");
+
+            // Mis 4 llamadas
+            Local l1 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Provincial l2 = new Provincial("Morón", Franja.Franja_1, 21, "Bernal");
+            Local l3 = new Local("Lanús", 45, "San Rafael", 1.99f);
+            Provincial l4 = new Provincial(Franja.Franja_3, l2);
+
+            // Las llamadas se irán registrando en la Centralita.
+            // La centralita mostrará por pantalla todas las llamadas según las vaya registrando.
+            c.Llamadas.Add(l1);
+            Console.WriteLine(c);
+            c.Llamadas.Add(l2);
+            Console.WriteLine(c);
+            c.Llamadas.Add(l3);
+            Console.WriteLine(c);
+            c.Llamadas.Add(l4);
+            Console.WriteLine(c);
+
+            c.OrdenarLlamadas();
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
