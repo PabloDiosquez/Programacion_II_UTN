8d6f8260af0b1fe2bb8fa293fb25ea0d7c40b796
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-18 18:22:24 -0300

    Se agrega Ejercicio_I01

---
+++ Clase_11/Biblioteca_EjercicioI01/CalculadoraDeStrings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Biblioteca_EjercicioI01
+{
+    /// <summary>
+    /// Clase que proporciona métodos para realizar operaciones matemáticas en cadenas de texto.
+    /// </summary>
+    public static class CalculadoraDeStrings
+    {
+        /// <summary>
+        /// Suma los números contenidos en una cadena de texto que puede contener hasta dos números separados por comas o saltos de línea.
+        /// </summary>
+        /// <param name="numeros">La cadena de texto que contiene los números a sumar.</param>
+        /// <returns>La suma de los números contenidos en la cadena de texto.</returns>
+        /// <exception cref="Exception">Se lanza si se encuentra un valor no válido o no entero en la cadena.</exception>
+        public static int Add(this string numeros)
+        {
+            if (numeros.Length <= 1) return 0;
+
+            char[] separadores = new char[] { ',', '\n' };
+
+            string[] numerosEnLista = numeros.Split(separadores, StringSplitOptions.RemoveEmptyEntries);
+
+            int sumaAlMomento = 0;
+
+            foreach (string numero in numerosEnLista)
+            {
+                if (!int.TryParse(numero, out int numeroSumado))
+                {
+                    throw new Exception("Asegúrese de ingresar números enteros.");
+                }
+
+                sumaAlMomento += numeroSumado;
+            }
+            return sumaAlMomento;
+        }
+    }
+    
+
+
+
+
+
+
+}
---
+++ Clase_11/Test_EjericioI01/Test_MetodoAdd.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca_EjercicioI01;
+
+namespace Test_EjericioI01
+{
+    [TestClass]
+    public class Test_MetodoAdd
+    {
+        [TestMethod]
+
+        public void Add_CuandoRecibeStringVacio_DeberiaRetornarElNumeroCero()
+        {
+            // Arrange
+
+            string str = string.Empty;
+
+            int resultadoEsperado = 0;
+
+            // Act 
+
+            int resultado = CalculadoraDeStrings.Add(str);    
+
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+
+        public void Add_CuandoRecibeUnUnicoNumero_DeberiaRetornarElNumeroCero()
+        {
+            // Arrange
+
+            string str = "1";
+
+            int resultadoEsperado = 0;
+
+            // Act 
+
+            int resultado = CalculadoraDeStrings.Add(str);
+
+            // Assert
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+
+        public void Add_CuandoRecibeDosNumerosSeparadosPorComa_DeberiaRetornarSuSumaNumerica()
+        {
+            // Arrange
+
+            string str = "1,2";
+
+            int resultadoEsperado = 3;
+
+            // Act 
+
+            int resultado = CalculadoraDeStrings.Add(str);
+
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        public void Add_CuandoRecibeTresNumerosSeparadosPorComa_DeberiaRetornarSuSumaNumerica()
+        {
+            // Arrange
+
+            string str = "1,2,3";
+
+            int resultadoEsperado = 6;
+
+            // Act 
+
+            int resultado = str.Add();
+            
+            // Assert 
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+    }
+}
