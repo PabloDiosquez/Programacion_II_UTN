757c1befbeedcb358e0a2263b6270937ddd59a5b
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-26 17:01:06 -0300

    Se agrega EjercicioI02 - Clase 13

---
+++ Clase_13_Interfaces/EjercicioI03_Biblioteca/Foto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI03_Biblioteca
+{
+    public class Foto : Imprimible
+    {
+        public void Imprimir() 
+        {
+
+        }
+    }
+}
---
+++ Clase_13_Interfaces/EjercicioI03_Biblioteca/Impresora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI03_Biblioteca
+{
+    public class Impresora
+    {
+        List<Imprimible> colaDeImpresion;
+
+        public Impresora()
+        {
+            colaDeImpresion = new List<Imprimible>();
+        }
+    }
+}
