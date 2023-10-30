79a179667c1b656ba73b38ef179cca07fe9a6ea2
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-10-30 10:39:34 -0300

    Se agrega EjercicioI01 - Clase 12

--- Clase_12_Generics/EjercicioI01_Biblioteca/Torneo.cs
+++ Clase_12_Generics/EjercicioI01_Biblioteca/Torneo.cs
@@ -4,51 +4,95 @@ using System.Text;
 
 namespace EjercicioI01_Biblioteca
 {
-    public class Torneo<T> 
-        where T: Equipo  
+    /// <summary>
+    /// Clase genérica que representa un torneo que acepta equipos de tipo T, donde T debe ser una subclase de la clase Equipo.
+    /// </summary>
+    /// <typeparam name="T">El tipo de equipos que se aceptarán en el torneo.</typeparam>
+    public class Torneo<T>
+        where T : Equipo
     {
-        // Atributos 
+        // Atributos
 
+        /// <summary>
+        /// Nombre del torneo.
+        /// </summary>
         private string nombre;
 
+        /// <summary>
+        /// Lista de equipos que participan en el torneo.
+        /// </summary>
         private List<T> equipos;
 
-        // Constructor 
+        // Constructor
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase Torneo con el nombre especificado.
+        /// </summary>
+        /// <param name="nombre">El nombre del torneo.</param>
         public Torneo(string nombre)
         {
             this.nombre = nombre;
-
             equipos = new List<T>();
         }
 
-        // Propiedades 
+        // Propiedades
 
-        public string Nombre { get; set; }
+        /// <summary>
+        /// Obtiene o establece el nombre del torneo.
+        /// </summary>
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
 
-        public List<T> Equipos { get; }
+        /// <summary>
+        /// Obtiene la lista de equipos que participan en el torneo.
+        /// </summary>
+        public List<T> Equipos
+        {
+            get { return equipos; }
+        }
 
-        // Métodos de instancia 
+        // Métodos de instancia
 
+        /// <summary>
+        /// Comprueba si un equipo específico está inscrito en el torneo.
+        /// </summary>
+        /// <param name="torneo">El torneo en el que se realizará la comprobación.</param>
+        /// <param name="equipoBuscado">El equipo que se está buscando.</param>
+        /// <returns>True si el equipo está inscrito en el torneo, de lo contrario, false.</returns>
         public static bool operator ==(Torneo<T> torneo, Equipo equipoBuscado)
         {
             if (torneo.equipos.Count == 0 || equipoBuscado is not T) return false;
 
             foreach (Equipo equipo in torneo.equipos)
             {
-                if(equipo == equipoBuscado) return true;
+                if (equipo == equipoBuscado) return true;
             }
             return false;
         }
 
+        /// <summary>
+        /// Comprueba si un equipo específico no está inscrito en el torneo.
+        /// </summary>
+        /// <param name="torneo">El torneo en el que se realizará la comprobación.</param>
+        /// <param name="equipo">El equipo que se está buscando.</param>
+        /// <returns>True si el equipo no está inscrito en el torneo, de lo contrario, false.</returns>
         public static bool operator !=(Torneo<T> torneo, Equipo equipo)
         {
             return !(torneo == equipo);
         }
 
+        /// <summary>
+        /// Agrega un equipo al torneo si el equipo no está inscrito y es del tipo adecuado.
+        /// </summary>
+        /// <param name="torneo">El torneo al que se agregará el equipo.</param>
+        /// <param name="equipo">El equipo que se agregará.</param>
+        /// <returns>True si el equipo se agregó con éxito, de lo contrario, false.</returns>
         public static bool operator +(Torneo<T> torneo, Equipo equipo)
         {
-            if (torneo != equipo && equipo is T) 
+            if (torneo != equipo && equipo is T)
             {
                 torneo.equipos.Add((T)equipo);
 
@@ -57,9 +101,13 @@ namespace EjercicioI01_Biblioteca
             return false;
         }
 
-        // Métodos de instancia 
+        // Métodos de instancia
 
-        public string Mostrar() 
+        /// <summary>
+        /// Muestra los detalles del torneo, incluyendo su nombre y la lista de equipos participantes.
+        /// </summary>
+        /// <returns>Una cadena que representa los detalles del torneo.</returns>
+        public string Mostrar()
         {
             StringBuilder sb = new StringBuilder();
 
@@ -77,27 +125,44 @@ namespace EjercicioI01_Biblioteca
             return sb.ToString();
         }
 
-        public string CalcularPartido(T equipo1, T equipo2)
+        /// <summary>
+        /// Juega un partido aleatorio entre dos equipos en el torneo.
+        /// </summary>
+        /// <returns>Una cadena que representa el resultado del partido.</returns>
+        public string JugarPartido()
         {
-            if(equipo1 is EquipoFutbol && equipo2 is EquipoFutbol) return CalcularPartido(equipo1, equipo2);
 
-            if(equipo1 is EquipoBasquet && equipo2 is EquipoBasquet) return CalcularPartido(equipo1, equipo2);
+            if (this.Equipos.Count < 2) return "La lista debe tener al menos dos equipos";
+
+            while (true)
+            {
+                int indiceA = new Random().Next(0, this.Equipos.Count);
+
+                int indiceB = new Random().Next(0, this.Equipos.Count);
+
+                if (indiceA == indiceB) continue;
+
+                return CalcularPartido(equipos[indiceA], equipos[indiceB]);
+            }
+        }
+
+        private string CalcularPartido(T equipo1, T equipo2)
+        {
+            if (equipo1 is EquipoFutbol && equipo2 is EquipoFutbol) return CalcularPartido(equipo1, equipo2);
+
+            if (equipo1 is EquipoBasquet && equipo2 is EquipoBasquet) return CalcularPartido(equipo1, equipo2);
 
             return string.Empty;
         }
 
-
-
-
-        private string CalcularPartido(EquipoFutbol equipo1, EquipoFutbol equipo2) 
+        private string CalcularPartido(EquipoFutbol equipo1, EquipoFutbol equipo2)
         {
             return $"{equipo1.Nombre} [{new Random().Next(1, 11)}] -- {equipo2.Nombre} [{new Random().Next(1, 11)}]";
         }
 
-        private string CalcularPartido(EquipoBasquet equipo1, EquipoBasquet equipo2) 
+        private string CalcularPartido(EquipoBasquet equipo1, EquipoBasquet equipo2)
         {
             return $"{equipo1.Nombre} [{new Random().Next(1, 120)}] -- {equipo2.Nombre} [{new Random().Next(1, 120)}]";
         }
-
     }
 }
---
+++ Clase_12_Generics/EjercicioI01_Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EjercicioI01_Biblioteca;
+namespace EjercicioI01_Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Torneo<EquipoBasquet> torneoBasquet = new Torneo<EquipoBasquet>("tBasquet");
+            EquipoBasquet e1 = new EquipoBasquet("e1");
+            EquipoBasquet e2 = new EquipoBasquet("e2");
+
+            e1.FechaDeCreacion = new DateTime(1990, 12, 21);
+            e2.FechaDeCreacion = new DateTime(1993, 6, 12);
+
+            _ = torneoBasquet + e1;
+            _ = torneoBasquet + e2;
+
+            Console.WriteLine(torneoBasquet.Mostrar());
+        }
+    }
+}
