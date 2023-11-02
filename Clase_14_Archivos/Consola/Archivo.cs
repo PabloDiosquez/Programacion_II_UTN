8d15c8deda64b96b4af052679e122367af334af6
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-01 22:53:50 -0300

    Se agrega la clase Archivo - Clase 14

--- Clase_14_Archivos/Consola/Archivo.cs
+++ Clase_14_Archivos/Consola/Archivo.cs
@@ -7,33 +7,54 @@ using System.Threading.Tasks;
 
 namespace Consola
 {
-    public class Archivo
+    public static class Archivo
     {
+        // Ruta para el escritorio del usuario.
+        static string ruta;
+
+        // Ruta del directorio en el que se encuentra la aplicación.
+        static string rutaProyecto;
+
+        // Constructor estático de la clase Archivo.
+        static Archivo()
+        {
+            // Obtiene la ruta del escritorio del usuario.
+            ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Obtiene la ruta del directorio en el que se encuentra la aplicación.
+            rutaProyecto = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
         /// <summary>
-        /// Esta función crea un archivo de texto en el escritorio del usuario y escribe cuatro líneas de texto en él.
+        /// Esta función crea un archivo de texto en el escritorio del usuario y escribe tres líneas de texto en él.
         /// </summary>
         public static void Escribir()
         {
-            // Obtiene la ruta del escritorio del usuario.
-            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-            // Obtiene la ruta del directorio en el que se encuentra la aplicación.
-            string rutaProyecto = AppDomain.CurrentDomain.BaseDirectory;
-
             // Define el nombre del archivo a crear.
             string nombreArchivo = "prueba.txt";
 
-            // Crea un StreamWriter para escribir en el archivo de texto.
-            StreamWriter sw = new StreamWriter($"{ruta}/{nombreArchivo}");
+            // Crea la ruta completa del archivo utilizando la ruta del escritorio y el nombre del archivo.
+            string rutaCompleta = $"{ruta}/{nombreArchivo}";
 
-            // Escribe cuatro líneas de texto en el archivo.
-            sw.WriteLine("Línea 1");
-            sw.WriteLine("Línea 2");
-            sw.WriteLine("Línea 3");
-            sw.WriteLine("Línea 4");
+            try
+            {
+                // Abre un StreamWriter y escribe en el archivo.
+                using (StreamWriter sw = new StreamWriter(rutaCompleta))
+                {
+                    // Escribe tres líneas de texto en el archivo.
+                    sw.WriteLine("Línea de prueba 1");
+                    sw.WriteLine("Línea de prueba 2");
+                    sw.WriteLine("Línea de prueba 3");
+                }
+            }
+            catch (Exception)
+            {
+                // Captura cualquier excepción y lanza una excepción personalizada.
+                throw new Exception($"Error en el archivo {ruta}/{nombreArchivo}");
+            }
 
-            // Cierra el StreamWriter para liberar los recursos.
-            sw.Close();
+            // Muestra un mensaje de éxito en la consola.
+            Console.WriteLine("Archivo creado con éxito.");
         }
     }
 }
