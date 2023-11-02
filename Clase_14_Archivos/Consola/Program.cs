7dce1f1eb7c21e584c722568774db2eaa66695df
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-01 22:01:11 -0300

    Se agregan comentarios en la clase Program - Clase 14

--- Clase_14_Archivos/Consola/Program.cs
+++ Clase_14_Archivos/Consola/Program.cs
@@ -3,8 +3,15 @@ using System.IO;
 
 namespace Consola
 {
+    /// <summary>
+    /// Clase principal del programa.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Punto de entrada del programa.
+        /// </summary>
+        /// <param name="args">Argumentos de línea de comandos.</param>
         static void Main(string[] args)
         {
             verificarSistemaOperativo();
@@ -18,34 +25,34 @@ namespace Consola
             CrearDirectorio();
         }
 
+        /// <summary>
+        /// Verifica el sistema operativo y muestra información sobre él.
+        /// </summary>
         static void verificarSistemaOperativo()
         {
             Console.WriteLine($"¿Es Windows? {OperatingSystem.IsWindows()}");
-
             Console.WriteLine($"¿Es Linux? {OperatingSystem.IsLinux()}");
-
             Console.WriteLine($"¿Es MacOS? {OperatingSystem.IsMacOS()}");
-
             Console.WriteLine($"¿Es IOS? {OperatingSystem.IsIOS()}");
-
             Console.WriteLine($"¿Es Android? {OperatingSystem.IsAndroid()}");
         }
 
+        /// <summary>
+        /// Obtiene información del sistema operativo y la muestra.
+        /// </summary>
         static void ObtenerInformacionOS()
         {
             Console.WriteLine($"Sistema Operativo: {Environment.OSVersion}");
-
             Console.WriteLine($"Plataforma: {Environment.OSVersion.Platform}");
-
             Console.WriteLine($"Versión: {Environment.OSVersion.Version}");
-
             Console.WriteLine($"Cantidad de procesadores lógicos: {Environment.ProcessorCount}");
-
             Console.WriteLine($"Arquitectura de 64 bits?: {Environment.Is64BitProcess}");
-
             Console.WriteLine($"Nombre de Usuario: {Environment.UserName}");
         }
 
+        /// <summary>
+        /// Obtiene información de almacenamiento y la muestra.
+        /// </summary>
         static void ObtenerInformacionAlmacenamiento()
         {
             DriveInfo[] volumenes = DriveInfo.GetDrives();
@@ -60,10 +67,12 @@ namespace Consola
             }
         }
 
+        /// <summary>
+        /// Crea un directorio en el escritorio del usuario.
+        /// </summary>
         static void CrearDirectorio()
         {
             string rutaEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
             string rutaNuevoEscritorio = Path.Combine(rutaEscritorio, "Clase 14 Archivos -- Prueba");
 
             VerificarSiExisteDirectorio(rutaNuevoEscritorio);
@@ -73,6 +82,10 @@ namespace Consola
             VerificarSiExisteDirectorio(rutaNuevoEscritorio);
         }
 
+        /// <summary>
+        /// Verifica si un directorio existe y muestra el resultado.
+        /// </summary>
+        /// <param name="rutaEscritorio">La ruta del directorio a verificar.</param>
         static void VerificarSiExisteDirectorio(string rutaEscritorio)
         {
             Console.WriteLine($"¿Existe el directorio? {Directory.Exists(rutaEscritorio)}");
