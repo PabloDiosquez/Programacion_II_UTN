099b43b9be57c43e0af51ffa9dd41dbc7e95b1fb
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-11 11:20:51 -0300

    Actualización Clase SerializaciónJSON

--- Clase15_SerializacionJSON/Clase15_SerializacionJSON/ClaseSerializadora.cs
+++ Clase15_SerializacionJSON/Clase15_SerializacionJSON/ClaseSerializadora.cs
@@ -8,16 +8,28 @@ using System.Threading.Tasks;
 
 namespace Clase15_SerializacionJSON
 {
+    /// <summary>
+    /// Clase estática para serializar y deserializar objetos a/desde archivos JSON.
+    /// </summary>
     public static class ClaseSerializadora<T>
     {
         // Atributos 
 
+        /// <summary>
+        /// Ruta del directorio base.
+        /// </summary>
         static string ruta;
 
+        /// <summary>
+        /// Ruta completa para el directorio de archivos JSON.
+        /// </summary>
         static string rutaDir;
 
         // Constructor 
 
+        /// <summary>
+        /// Constructor estático que inicializa la ruta base y el directorio para los archivos JSON.
+        /// </summary>
         static ClaseSerializadora()
         {
             ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
@@ -25,34 +37,63 @@ namespace Clase15_SerializacionJSON
             rutaDir = @$"{ruta}\ArchivosJSON";
         }
 
+        /// <summary>
+        /// Método estático para escribir datos serializados en un archivo JSON.
+        /// </summary>
+        /// <param name="datos">Datos a ser serializados.</param>
+        /// <param name="path">Ruta del archivo en la que se escribirán los datos serializados.</param>
         public static void Escribir(T datos, string path)
         {
             try
             {
                 if (!Directory.Exists(rutaDir))
                 {
+                    // Verifica si el directorio no existe, de lo contrario lo crea
                     Directory.CreateDirectory(rutaDir);
                 }
 
+                // Obtiene la ruta completa del archivo
                 string fullPath = Path.Combine(rutaDir, path);
 
+                // Serializa el objeto a formato JSON
                 string objetoJson = JsonSerializer.Serialize(datos);
 
+                // Escribe el JSON en el archivo
                 File.WriteAllText(fullPath, objetoJson);
-
             }
             catch (Exception ex)
             {
+                // Captura cualquier excepción y la lanza como una nueva excepción con un mensaje personalizado
                 throw new Exception($"Algo salió mal: {ex.Message}");
             }
         }
 
+        /// <summary>
+        /// Método estático para leer datos desde un archivo JSON y deserializarlos.
+        /// </summary>
+        /// <param name="path">Ruta del archivo JSON a leer.</param>
+        /// <returns>Los datos deserializados.</returns>
         public static T Leer(string path)
         {
+            T objetoRecuperado = default(T);
 
+            try
+            {
+                // Combina la ruta base con la ruta del archivo
+                path = Path.Combine(rutaDir, path);
 
+                // Lee el JSON desde el archivo
+                string json = File.ReadAllText(path);
 
-            return default(T);
+                // Deserializa el JSON y asigna los datos al objeto recuperado
+                objetoRecuperado = JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                // Captura cualquier excepción y la lanza como una nueva excepción con un mensaje personalizado
+                throw new Exception($"Algo salió mal: {ex.Message}");
+            }
+            return objetoRecuperado;
         }
     }
 }
--- Clase15_SerializacionJSON/Clase15_SerializacionJSON/Program.cs
+++ Clase15_SerializacionJSON/Clase15_SerializacionJSON/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Clase15_SerializacionJSON
 {
@@ -6,7 +7,21 @@ namespace Clase15_SerializacionJSON
     {
         static void Main(string[] args)
         {
-            
+            Mascota mascota1 = new Mascota("Coco", 11, "Callejero", 5.2, true);
+            Mascota mascota2 = new Mascota("Luchi", 6, "Boxer", 11.1, true);
+
+            //ClaseSerializadora<Mascota>.Escribir(mascota1, "mascota.json");
+
+            List<Mascota> mascotas = new List<Mascota>() { mascota1, mascota2 };
+
+            //ClaseSerializadora<List<Mascota>>.Escribir(mascotas, "mascotas.json");
+
+            List<Mascota> mascotasRecuperadas = ClaseSerializadora<List<Mascota>>.Leer("mascotas.json");
+
+            foreach (Mascota mascota in mascotasRecuperadas)
+            {
+                Console.WriteLine(mascota);
+            }
         }
     }
 }
