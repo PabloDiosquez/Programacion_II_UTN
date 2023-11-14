03cabcb668bdc632463af52f53b6eeda9a7f25f9
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-14 09:29:09 -0300

    Actualización Ejercicio I01

--- Clase_15/Ejercicio_I01/FrmListaSuper.cs
+++ Clase_15/Ejercicio_I01/FrmListaSuper.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Ejercicio_I01
 {
@@ -23,8 +25,8 @@ namespace Ejercicio_I01
         private void FrmListaSuper_Load(object sender, EventArgs e)
         {
             SetToolTip();
-            //CargarListaAlmacenada();
-            //RefrescarLista();
+
+            CargarListaAlmacenada();
         }
 
         private void btn_Agregar_Click(object sender, EventArgs e)
@@ -57,10 +59,61 @@ namespace Ejercicio_I01
             this.toolTip.SetToolTip(this.btn_Modificar, "Modificar objeto");
         }
 
-        // TODO 1: Empezar por acá... 
         private void CargarListaAlmacenada()
         {
-             
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string pathDir = $@"{path}\ArchivosSuper";
+
+            // Esta línea revisa si el directorio existe. En caso contrario, lo crea.
+            if (!Directory.Exists(pathDir)) Directory.CreateDirectory(pathDir);
+
+            try
+            {
+                string pathArchivo = Path.Combine(pathDir, "listaSuper.xml");
+
+                if (File.Exists(pathArchivo))
+                {
+                    using (StreamReader sr = new StreamReader(pathArchivo))
+                    {
+                        XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+
+                        List<string>? listaSuper = xmlSerializer.Deserialize(sr) as List<string>;
+
+                        if (listaSuper is not null)
+                        {
+                            this.lbx_ListaSuper.DataSource = listaSuper;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MostrarMensajeError(ex);
+            }
+        }
+
+        private void GuardarCambios()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string pathDir = Path.Combine(path, "ArchivosSuper");
+
+            try
+            {
+                string pathArchivo = Path.Combine(pathDir, "listaSuper.xml");
+
+                using (StreamWriter sw = new StreamWriter(pathArchivo))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+
+                    xmlSerializer.Serialize(sw, this.listaSuper);
+                }
+            }
+            catch (Exception ex)
+            {
+                MostrarMensajeError(ex);    
+            }
         }
 
         private void RefrescarLista()
@@ -79,6 +132,8 @@ namespace Ejercicio_I01
                 this.listaSuper.Add(frmAlta.Objeto);
 
                 RefrescarLista();
+
+                GuardarCambios();
             }
         }
 
@@ -91,6 +146,8 @@ namespace Ejercicio_I01
                 this.listaSuper.Remove(objeto);
 
                 RefrescarLista();
+
+                GuardarCambios();
             }
             else 
             {
@@ -111,11 +168,23 @@ namespace Ejercicio_I01
                 this.listaSuper[indice] = frmAlta.Objeto;
 
                 RefrescarLista();
+
+                GuardarCambios();
             }
             else
             {
                 MessageBox.Show("Asegúrese de elegir un elemento de la lista 🤔", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);  ;
             }
+        }
+
+        public void MostrarMensajeError(Exception ex)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Ocurrió un error al cargar el archivo XML");
+            sb.AppendLine(ex.Message);
+            sb.AppendLine(ex.StackTrace);
+
+            MessageBox.Show(sb.ToString(), "Error al cargar el archivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
