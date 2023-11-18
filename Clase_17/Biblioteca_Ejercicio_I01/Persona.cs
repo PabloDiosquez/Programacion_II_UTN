ab49972da8f282bcd1bb359a56aa327801b4a98e
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-18 11:55:08 -0300

    Actualización Ejercicio_I01

--- Clase_17/Biblioteca_Ejercicio_I01/Persona.cs
+++ Clase_17/Biblioteca_Ejercicio_I01/Persona.cs
@@ -2,6 +2,9 @@
 
 namespace Biblioteca_Ejercicio_I01
 {
+    /// <summary>
+    /// Clase que representa a una Persona con atributos como Id, Nombre y Apellido.
+    /// </summary>
     public class Persona
     {
         // Atributos 
@@ -14,29 +17,54 @@ namespace Biblioteca_Ejercicio_I01
 
         // Constructores 
 
+        /// <summary>
+        /// Constructor que inicializa una nueva instancia de la clase Persona con un Id, Nombre y Apellido.
+        /// </summary>
+        /// <param name="id">El identificador único de la Persona.</param>
+        /// <param name="nombre">El nombre de la Persona.</param>
+        /// <param name="apellido">El apellido de la Persona.</param>
         public Persona(int id, string nombre, string apellido) : this(nombre, apellido)
         {
             this.id = id;
         }
 
+        /// <summary>
+        /// Constructor que inicializa una nueva instancia de la clase Persona con un Nombre y Apellido.
+        /// </summary>
+        /// <param name="nombre">El nombre de la Persona.</param>
+        /// <param name="apellido">El apellido de la Persona.</param>
         public Persona(string nombre, string apellido)
         {
             this.nombre = nombre;
-
             this.apellido = apellido;
         }
 
         // Propiedades 
+
+        /// <summary>
+        /// Obtiene o establece el Id de la Persona.
+        /// </summary>
         public int Id { get => id; set => id = value; }
+
+        /// <summary>
+        /// Obtiene o establece el Nombre de la Persona.
+        /// </summary>
         public string Nombre { get => nombre; set => nombre = value; }
+
+        /// <summary>
+        /// Obtiene o establece el Apellido de la Persona.
+        /// </summary>
         public string Apellido { get => apellido; set => apellido = value; }
 
         // Sobrecarga del método ToString().
 
+        /// <summary>
+        /// Devuelve una representación de cadena de la Persona.
+        /// </summary>
+        /// <returns>Una cadena que representa la Persona.</returns>
         public override string ToString()
         {
             return $"[Id: {this.Id}, Nombre: {this.Nombre}, Apellido: {this.Apellido}]";
         }
-
     }
 }
--- Clase_17/Biblioteca_Ejercicio_I01/PersonaDAO.cs
+++ Clase_17/Biblioteca_Ejercicio_I01/PersonaDAO.cs
@@ -11,124 +11,170 @@ namespace Biblioteca_Ejercicio_I01
     {
         static string conexionString;
 
-        static SqlConnection conexion;
-
-        static SqlCommand comando;
-
         static PersonaDAO()
         {
             conexionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=BD_I01;Integrated Security=True";
-
-            conexion = new SqlConnection(conexionString);
-
-            comando = new SqlCommand();
-
-            comando.Connection = conexion;
-
-            comando.CommandType = System.Data.CommandType.Text;
         }
 
+        /// <summary>
+        /// Guarda una nueva Persona en la base de datos.
+        /// </summary>
+        /// <param name="persona">La Persona a guardar en la base de datos.</param>
         public static void Guardar(Persona persona)
         {
             try
             {
-                conexion.Open();
-                comando.Parameters.Clear();
-                comando.CommandText = $"INSERT INTO PERSONA (ID, NOMBRE, APELLIDO) VALUES (@id, @nombre, @apellido)";
-                comando.Parameters.AddWithValue("@id", persona.Id);
-                comando.Parameters.AddWithValue("@nombre", persona.Nombre);
-                comando.Parameters.AddWithValue("@apellido", persona.Apellido);
+                // Utilizando 'using' para garantizar la liberación de recursos
+                using (SqlConnection conexion = new SqlConnection(conexionString))
+                {
+                    // Abre la conexión
+                    conexion.Open();
 
-                comando.ExecuteNonQuery();
+                    // Utilizando 'using' para garantizar la liberación de recursos
+                    using (SqlCommand comando = new SqlCommand())
+                    {
+                        comando.CommandText = $"INSERT INTO PERSONA (ID, NOMBRE, APELLIDO) VALUES (@id, @nombre, @apellido)";
+                        comando.Parameters.AddWithValue("@id", persona.Id);
+                        comando.Parameters.AddWithValue("@nombre", persona.Nombre);
+                        comando.Parameters.AddWithValue("@apellido", persona.Apellido);
+
+                        comando.ExecuteNonQuery();
+                    }
+                }
             }
-            catch (Exception)
+            catch (SqlException ex)
             {
-                throw;
+                // Manejar o registrar la excepción específica de SQL
+                Console.WriteLine("Error de SQL: " + ex.Message);
             }
-            finally
+            catch (Exception ex)
             {
-                conexion.Close();
+                // Manejar o registrar otras excepciones
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
 
+        /// <summary>
+        /// Lee una Persona de la base de datos por su ID.
+        /// </summary>
+        /// <param name="id">El ID de la Persona a leer.</param>
+        /// <returns>La Persona leída o default(Persona) si no se encuentra.</returns>
         public static Persona LeerPorId(int id)
         {
             try
             {
-                conexion.Open();
-                comando.Parameters.Clear();
-                comando.CommandText = $"SELECT * FROM PERSONA WHERE ID = @ID_PERSONA;";
-                comando.Parameters.AddWithValue("@ID_Persona", id);
-
-                SqlDataReader lector = comando.ExecuteReader();
-
-                if (lector.Read())
+                // Utilizando 'using' para garantizar la liberación de recursos
+                using (SqlConnection conexion = new SqlConnection(conexionString))
                 {
-                    return new Persona(Convert.ToInt32(lector["ID"]), lector["NOMBRE"].ToString(), lector["APELLIDO"].ToString());
+                    // Abre la conexión
+                    conexion.Open();
+
+                    // Utilizando 'using' para garantizar la liberación de recursos
+                    using (SqlCommand comando = new SqlCommand())
+                    {
+                        comando.CommandText = $"SELECT * FROM PERSONA WHERE ID = @ID_PERSONA;";
+                        comando.Parameters.AddWithValue("@ID_Persona", id);
+
+                        SqlDataReader lector = comando.ExecuteReader();
+
+                        if (lector.Read())
+                        {
+                            return new Persona(Convert.ToInt32(lector["ID"]), lector["NOMBRE"].ToString(), lector["APELLIDO"].ToString());
+                        }
+                    }
                 }
-                return default(Persona);
             }
-            catch (Exception)
+            catch (SqlException ex)
             {
-                throw;
+                // Manejar o registrar la excepción específica de SQL
+                Console.WriteLine("Error de SQL: " + ex.Message);
             }
-            finally
+            catch (Exception ex)
             {
-                conexion.Close();
+                // Manejar o registrar otras excepciones
+                Console.WriteLine("Error: " + ex.Message);
             }
+            return default(Persona);
         }
 
+        /// <summary>
+        /// Lee todas las Personas de la base de datos.
+        /// </summary>
+        /// <returns>Una lista de Personas leídas de la base de datos.</returns>
         public static List<Persona> Leer()
         {
-            List<Persona> personas = new List<Persona>();    
+            List<Persona> personas = new List<Persona>();
 
             try
             {
-                conexion.Open();
-
-                comando.CommandText = "SELECT * FROM PERSONA;";
-
-                SqlDataReader lector = comando.ExecuteReader();
-
-                while (lector.Read())
+                // Utilizando 'using' para garantizar la liberación de recursos
+                using (SqlConnection conexion = new SqlConnection(conexionString))
                 {
-                    personas.Add(new Persona(Convert.ToInt32(lector["ID"]), lector["NOMBRE"].ToString(), lector["APELLIDO"].ToString()));
+                    // Abre la conexión
+                    conexion.Open();
+
+                    // Utilizando 'using' para garantizar la liberación de recursos
+                    using (SqlCommand comando = new SqlCommand("SELECT * FROM PERSONA"))
+                    {
+                        SqlDataReader lector = comando.ExecuteReader();
+
+                        while (lector.Read())
+                        {
+                            personas.Add(new Persona(Convert.ToInt32(lector["ID"]), lector["NOMBRE"].ToString(), lector["APELLIDO"].ToString()));
+                        }
+                    }
                 }
             }
-            catch (Exception)
+            catch (SqlException ex)
             {
-                throw;
+                // Manejar o registrar la excepción específica de SQL
+                Console.WriteLine("Error de SQL: " + ex.Message);
             }
-            finally 
+            catch (Exception ex)
             {
-                conexion.Close();
+                // Manejar o registrar otras excepciones
+                Console.WriteLine("Error: " + ex.Message);
             }
-            return personas;    
+            return personas;
         }
 
-        public static void Modificar(Persona personaAntigua, Persona personaNueva) 
+        /// <summary>
+        /// Modifica una Persona en la base de datos.
+        /// </summary>
+        /// <param name="personaAntigua">La Persona antigua que se va a modificar.</param>
+        /// <param name="personaNueva">La nueva información de la Persona.</param>
+        public static void Modificar(Persona personaAntigua, Persona personaNueva)
         {
             try
             {
-                if (conexion.State == System.Data.ConnectionState.Closed) conexion.Open();
+                // Utilizando 'using' para garantizar la liberación de recursos
+                using (SqlConnection conexion = new SqlConnection(conexionString))
+                {
+                    // Abre la conexión
+                    conexion.Open();
 
-                comando.Parameters.Clear();
+                    // Utilizando 'using' para garantizar la liberación de recursos
+                    using (SqlCommand comando = new SqlCommand())
+                    {
+                        comando.CommandText = "UPDATE PERSONA SET ID=@nuevoId, NOMBRE=@nuevoNombre, APELLIDO=@nuevoApellido WHERE ID=@idAntiguo";
+                        comando.Parameters.AddWithValue("@nuevoId", personaNueva.Id);
+                        comando.Parameters.AddWithValue("@nuevoNombre", personaNueva.Nombre);
+                        comando.Parameters.AddWithValue("@nuevoApellido", personaNueva.Apellido);
+                        comando.Parameters.AddWithValue("@idAntiguo", personaAntigua.Id);
 
-                comando.CommandText = "UPDATE PERSONA SET ID=@nuevoId, NOMBRE=@nuevoNombre, APELLIDO=@nuevoApellido WHERE ID=@idAntiguo";
-                comando.Parameters.AddWithValue("@nuevoId", personaNueva.Id);
-                comando.Parameters.AddWithValue("@nuevoNombre", personaNueva.Nombre);
-                comando.Parameters.AddWithValue("@nuevoApellido", personaNueva.Apellido);
-                comando.Parameters.AddWithValue("@idAntiguo", personaAntigua.Id);
-
-                comando.ExecuteNonQuery();
+                        comando.ExecuteNonQuery();
+                    }
+                }
             }
-            catch (Exception)
+            catch (SqlException ex)
             {
-                throw;
+                // Manejar o registrar la excepción específica de SQL
+                Console.WriteLine("Error de SQL: " + ex.Message);
             }
-            finally
+            catch (Exception ex)
             {
-                conexion.Close();
+                // Manejar o registrar otras excepciones
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
 
