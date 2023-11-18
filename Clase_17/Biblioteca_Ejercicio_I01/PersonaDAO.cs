584ab8716a5cc9c8d54929a1e1b256727aac8873
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-18 10:22:23 -0300

    Actualización Ejercicio_I01

--- Clase_17/Biblioteca_Ejercicio_I01/PersonaDAO.cs
+++ Clase_17/Biblioteca_Ejercicio_I01/PersonaDAO.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Biblioteca_Ejercicio_I01
 {
-    internal class PersonaDAO
+    public static class PersonaDAO
     {
+        static string conexionString;
+
+        static SqlConnection conexion;
+
+        static SqlCommand comando;
+
+        static PersonaDAO()
+        {
+            conexionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=BD_I01;Integrated Security=True";
+
+            conexion = new SqlConnection(conexionString);
+
+            comando = new SqlCommand();
+
+            comando.Connection = conexion;
+
+            comando.CommandType = System.Data.CommandType.Text;
+        }
+
+        public static void Guardar(Persona persona)
+        {
+            try
+            {
+                conexion.Open();
+                comando.Parameters.Clear();
+                comando.CommandText = $"INSERT INTO PERSONA (ID, NOMBRE, APELLIDO) VALUES (@ID, @NOMBRE, @APELLIDO)')";
+                comando.Parameters.AddWithValue("@ID", persona.Id);
+                comando.Parameters.AddWithValue("@NOMBRE", persona.Nombre);
+                comando.Parameters.AddWithValue("@APELLIDO", persona.Apellido);
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public static Persona LeerPorId(int id)
+        {
+            try
+            {
+                conexion.Open();
+                comando.Parameters.Clear();
+                comando.CommandText = $"SELECT * FROM PERSONA WHERE ID = @ID_PERSONA;";
+                comando.Parameters.AddWithValue("@ID_Persona", id);
+
+                SqlDataReader lector = comando.ExecuteReader();
+
+                if (lector.Read())
+                {
+                    return new Persona(Convert.ToInt32(lector["ID"]), lector["NOMBRE"].ToString(), lector["APELLIDO"].ToString());
+                }
+                return default(Persona);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+
+
+
     }
 }
