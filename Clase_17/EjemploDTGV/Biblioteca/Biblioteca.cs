eb5f894d44f815333122859a3f00db313d25be0f
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-19 15:04:09 -0300

    Actualización Ejercicio_I03

---
+++ Clase_17/EjemploDTGV/Biblioteca/Biblioteca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Biblioteca
+    {
+        // Atributos 
+
+        private int codigoJuego;
+
+        private string genero;
+
+        private string juego;
+
+        private string usuario;
+
+        // Constructor 
+
+        public Biblioteca(string usuario, string genero, string juego, int codigoJuego) 
+        {
+            this.usuario = usuario;
+
+            this.genero = genero;
+
+            this.juego = juego;
+
+            this.codigoJuego = codigoJuego;
+        }
+
+        // Propiedades 
+        public int CodigoJuego { get => codigoJuego; }
+        public string Genero { get => genero; }
+        public string Juego { get => juego; }
+        public string Usuario { get => usuario; }
+    }
+}
