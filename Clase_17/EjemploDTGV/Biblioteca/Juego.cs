1c36c3114d2d0c8c73755c999a39aed8a4f91213
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-19 13:36:26 -0300

    Actualización Ejercicio_I03

---
+++ Clase_17/EjemploDTGV/Biblioteca/Juego.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Juego
+    {
+        // Atributos 
+
+        private int codigoJuego;
+
+        private int codigoUsuario;
+
+        private string genero;
+
+        private string nombre;
+
+        private double precio;
+
+        // Constructores 
+
+        public Juego(string nombre, double precio, string genero, int codigoJuego, int codigoUsuario) 
+            : this(nombre, precio, genero, codigoUsuario) 
+        {
+            this.codigoJuego = codigoJuego; 
+        }
+
+        public Juego(string nombre, double precio, string genero, int codigoUsuario)
+        {
+            this.nombre = nombre;   
+
+            this.precio = precio;   
+
+            this.genero = genero;   
+
+            this.codigoUsuario = codigoUsuario;
+        }
+
+        // Propiedades 
+
+        public int CodigoJuego { get => codigoJuego; }
+        public int CodigoUsuario { get => codigoUsuario; }
+        public string Genero { get => genero; }
+        public string Nombre { get => nombre; }
+        public double Precio { get => precio; }
+    }
+}
---
+++ Clase_17/EjemploDTGV/Biblioteca/JuegoDAO.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class JuegoDAO
+    {
+        // Atributos 
+
+        static string cadenaConexion;
+
+        static SqlConnection conexion;
+
+        static SqlCommand comando;
+
+        // Constructor
+
+        static JuegoDAO()
+        {
+            cadenaConexion = @"Data Source=.\SQLEXPRESS;Initial Catalog=EJERCICIOS_UTN;Integrated Security=true";
+
+            conexion = new SqlConnection(cadenaConexion);
+
+            comando = new SqlCommand();
+
+            comando.Connection = conexion;
+        }
+    }
+}
