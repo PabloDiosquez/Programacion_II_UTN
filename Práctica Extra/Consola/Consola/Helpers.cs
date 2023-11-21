c895695f92e5c6ec989c3936e3b3664fb9c2a605
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-21 16:28:00 -0300

    Actualización Práctica Extra

---
+++ Práctica Extra/Consola/Consola/Helpers.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public static class Helpers
+    {
+        public static int ContarLetras(string texto)
+        {
+            int cantidadLetrasAlMomento = 0;
+
+            foreach (string palabra in texto.Split(' ', options: StringSplitOptions.RemoveEmptyEntries))
+            {
+                cantidadLetrasAlMomento += palabra.Length;
+            }
+            return cantidadLetrasAlMomento;
+        }
+
+        public static int ValidarNumeroEntero()
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número entero:");
+
+            while (!int.TryParse(Console.ReadLine(), out numero)) Console.WriteLine("Asegúrese de ingresar un número entero...");
+
+            return numero;
+        }
+
+        public static double ValidarNumero()
+        {
+            double numero;
+
+            Console.WriteLine("Ingrese un número real:");
+
+            while (!double.TryParse(Console.ReadLine(), out numero)) Console.WriteLine("Asegúrese de ingresar un número...");
+            
+            return numero;
+        }
+
+        public static double CalcularPromedio(int[] numeros)
+        {
+            double total = 0;
+
+            foreach (int numero in numeros)
+            {
+                total += numero;
+            }
+            return total / numeros.Length; 
+        }
+
+        public static int Maximo(int[] numeros)
+        {
+            int maximoAlMomento = numeros[0];
+
+            foreach (int numero in numeros)
+            {
+                maximoAlMomento = MaximoEntre(maximoAlMomento, numero);
+            }
+            return maximoAlMomento;
+        }
+
+        public static int Minimo(int[] numeros)
+        {
+            int minimoAlMomento = numeros[0];
+
+            foreach (int numero in numeros)
+            {
+                minimoAlMomento = MinimoEntre(minimoAlMomento, numero);
+            }
+            return minimoAlMomento;
+        }
+
+        public static int MaximoEntre(int primerNumero, int segundoNumero)
+        {
+            if (primerNumero >= segundoNumero) return primerNumero;
+            
+            return segundoNumero;
+        }
+
+        public static int MinimoEntre(int primerNumero, int segundoNumero) 
+        {
+            if (primerNumero <= segundoNumero) return primerNumero;
+
+            return segundoNumero;
+        }
+    }
+}
---
+++ Práctica Extra/Consola/Consola/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Consola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            
+            
+
+
+
+
+
+
+
+
+        }
+
+        // Ejercicio 1.
+        //Escriba un programa de aplicación que solicite al usuario ingresar su nombre y apellido,
+        //e imprima en la consola un saludo personalizado utilizando ese nombre e informe entre()
+        //la cantidad de letras que conforman la oración
+        //impresa.
+        //Ejemplo: Hola Juan Perez, bienvenido a la materia (34)
+
+        public static Action<string> saludar = nombreCompleto => 
+        {
+            string mensaje = $"Hola {nombreCompleto}, bienvenido a la materia";
+
+            Console.WriteLine($"{mensaje} ({Helpers.ContarLetras(mensaje)})");
+        };
+
+        // Ejercicio 2.
+        //Escriba un programa de aplicación que calcule la suma de dos números reales introducidos por
+        //teclado y muestre el resultado en la consola.
+
+        public static Func<double, double, double> suma = ((x, y) => x + y);
+
+        public static void PedirDosNumerosYMostrarSuSuma()
+        {
+            double primerNumero = Helpers.ValidarNumero();
+
+            double segundoNumero = Helpers.ValidarNumero();
+
+            Console.WriteLine($"Suma: {suma(primerNumero, segundoNumero)}");
+        }
+
+        // Ejercicio 3.
+        //Escriba un programa de aplicación que lea por teclado una secuencia de números y que imprima
+        //el valor máximo, el valor mínimo de dicho conjunto y el valor promedio.
+
+        public static Func<int[], int> minimo = numeros => Helpers.Minimo(numeros);
+
+        public static Func<int[], int> maximo = numeros => Helpers.Maximo(numeros);
+
+        public static Func<int[], double> promedio = numeros => Helpers.CalcularPromedio(numeros);
+
+        public static Action<int> leerYMostrar = cantidad => MostrarDatosEnArray(LeerPorTeclado(cantidad));
+
+        public static int[] LeerPorTeclado(int cantidad)
+        {
+            int[] numeros = new int[cantidad];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = Helpers.ValidarNumeroEntero();
+            }
+            return numeros;
+        }
+
+        public static void MostrarDatosEnArray(int[] numeros)
+        {
+            Console.WriteLine($"Mínimo: {minimo(numeros)}");
+
+            Console.WriteLine($"Máximo: {maximo(numeros)}");
+
+            Console.WriteLine($"Promedio: {promedio(numeros)}");
+        }
+
+    }
+}
