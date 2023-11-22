27624a32ed768194a9a244a7237740a378dadb94
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-22 17:49:33 -0300

    Actualización Libro_02

---
+++ Práctica Extra/LibroDeTrabajo_02/LibroDeTrabajo_02/Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroDeTrabajo_02
+{
+    public static class Helpers
+    {
+        public static bool EsPalindromo(this string str)
+        {
+            if (str is null) throw new ArgumentNullException("str");
+
+            return str.Replace(" ", "").ToLower() == str.Replace(" ", "").ToLower().RevertirStr();
+        }
+
+        public static string RevertirStr(this string str)
+        {
+            if (str is null) throw new ArgumentNullException("str");
+
+            string invertido = string.Empty;
+
+            for (int indice = str.Length - 1; indice >= 0; indice--)
+            {
+                invertido += str[indice];
+            }
+            return invertido;
+        }
+    }
+}
---
+++ Práctica Extra/LibroDeTrabajo_02/LibroDeTrabajo_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LibroDeTrabajo_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+
+        // Ejercicio 1.
+        //Escriba un programa de aplicación que lea una palabra y determine si es un palíndromo.
+        //(Ej: “abbccd” y “dccbba” son palíndromos).
+
+        static Predicate<string> esPalindromo = str => str.EsPalindromo();
+
+    }
+}
