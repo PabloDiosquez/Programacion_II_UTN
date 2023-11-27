b1e1d26c3f9b11c1c99578f4c13db8f9ac0db3df
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-27 14:45:40 -0300

    Libro de Trabajo 03

--- Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Competencia.cs
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Competencia.cs
@@ -6,63 +6,96 @@ using System.Threading.Tasks;
 
 namespace LibroDeTrabajo_03
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Clase que representa una competencia con alumnos inscritos.
+    /// </summary>
     public class Competencia
     {
         // Atributos
 
+        /// <summary>
+        /// Lista de alumnos inscritos en la competencia.
+        /// </summary>
         private List<Alumno> alumnosInscriptos;
 
+        /// <summary>
+        /// Total de alumnos inscritos en la competencia.
+        /// </summary>
         private int totalInscritos;
 
         // Constructor 
 
+        /// <summary>
+        /// Constructor predeterminado de la clase Competencia.
+        /// Inicializa la lista de alumnos inscritos y el total de inscritos.
+        /// </summary>
         public Competencia()
         {
             this.alumnosInscriptos = new List<Alumno>();
-
             this.totalInscritos = 0;
         }
 
         // Métodos de instancia 
 
-        public bool InscribirAlumno(Alumno alumno) 
+        /// <summary>
+        /// Inscribir a un alumno en la competencia.
+        /// </summary>
+        /// <param name="alumno">El alumno a inscribir.</param>
+        /// <returns>True si la inscripción fue exitosa, False si el alumno ya estaba inscrito.</returns>
+        public bool InscribirAlumno(Alumno alumno)
         {
             if (!this.ExisteAlumno(alumno))
             {
                 this.alumnosInscriptos.Add(alumno);
-
                 this.totalInscritos++;
-
-                return true;
-            } 
-            return false;
-            
-        }
-
-        public bool BorrarAlumno(Alumno alumno) 
-        {
-            if (this.ExisteAlumno(alumno))
-            {
-                this.alumnosInscriptos.Remove(alumno);
-
-                this.totalInscritos--;
-
                 return true;
             }
             return false;
         }
 
-        public int ObtenerTotalAlumnosInscriptos() 
+        /// <summary>
+        /// Borrar a un alumno de la competencia.
+        /// </summary>
+        /// <param name="alumno">El alumno a borrar.</param>
+        /// <returns>True si el borrado fue exitoso, False si el alumno no estaba inscrito.</returns>
+        public bool BorrarAlumno(Alumno alumno)
+        {
+            if (this.ExisteAlumno(alumno))
+            {
+                this.alumnosInscriptos.Remove(alumno);
+                this.totalInscritos--;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Obtener el total de alumnos inscritos en la competencia.
+        /// </summary>
+        /// <returns>El total de alumnos inscritos.</returns>
+        public int ObtenerTotalAlumnosInscriptos()
         {
             return this.totalInscritos;
         }
 
+        /// <summary>
+        /// Obtener el total de escuelas representadas por los alumnos inscritos.
+        /// </summary>
+        /// <returns>El total de escuelas.</returns>
         public int ObtenerTotalEscuelas()
         {
             return this.ObtenerEscuelas().Count();
         }
 
-        public string ImprimirEscuelas() 
+        /// <summary>
+        /// Imprimir la lista de escuelas inscritas en la competencia.
+        /// </summary>
+        /// <returns>Una cadena que representa las escuelas inscritas.</returns>
+        public string ImprimirEscuelas()
         {
             StringBuilder sb = new StringBuilder();
 
@@ -80,6 +113,12 @@ namespace LibroDeTrabajo_03
             return sb.ToString();
         }
 
+        // Métodos privados
+
+        /// <summary>
+        /// Obtener una lista de escuelas representadas por los alumnos inscritos.
+        /// </summary>
+        /// <returns>Una lista de nombres de escuelas.</returns>
         private List<string> ObtenerEscuelas()
         {
             List<string> escuelas = new List<string>();
@@ -94,7 +133,12 @@ namespace LibroDeTrabajo_03
             return escuelas;
         }
 
-        private bool ExisteAlumno(Alumno alumno) 
+        /// <summary>
+        /// Verificar si un alumno ya está inscrito en la competencia.
+        /// </summary>
+        /// <param name="alumno">El alumno a verificar.</param>
+        /// <returns>True si el alumno está inscrito, False si no lo está.</returns>
+        private bool ExisteAlumno(Alumno alumno)
         {
             foreach (Alumno alumnoInscripto in this.alumnosInscriptos)
             {
--- Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Helpers.cs
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Helpers.cs
@@ -6,29 +6,63 @@ using System.Threading.Tasks;
 
 namespace LibroDeTrabajo_03
 {
+    /// <summary>
+    /// Clase estática de ayuda con métodos para manipular cadenas.
+    /// </summary>
     public static class Helpers
     {
-        public static bool EsPalindromo(string str) 
+        /// <summary>
+        /// Verifica si una cadena es un palíndromo.
+        /// </summary>
+        /// <param name="str">La cadena a verificar.</param>
+        /// <returns>True si la cadena es un palíndromo, False si no lo es.</returns>
+        public static bool EsPalindromo(this string str)
         {
-            return string.Join(string.Empty, str.Split(' ', StringSplitOptions.RemoveEmptyEntries)) == InvertirTexto(str);
+            // Utiliza los métodos de extensión para remover espacios en blanco e invertir el texto
+            return str.RemoverEspaciosEnBlanco() == str.InvertirTexto();
         }
 
-        private static string InvertirTexto(string str)
+        /// <summary>
+        /// Invierte el orden de los caracteres en una cadena.
+        /// </summary>
+        /// <param name="str">La cadena a invertir.</param>
+        /// <returns>La cadena invertida.</returns>
+        private static string InvertirTexto(this string str)
         {
-            string[] palabras = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            return InvertirCaracteres(string.Join(string.Empty, palabras));
+            // Utiliza el método de extensión para remover espacios en blanco y luego invierte los caracteres
+            return str.RemoverEspaciosEnBlanco().InvertirCaracteres();
         }
 
-        private static string InvertirCaracteres(string str) 
+        /// <summary>
+        /// Remueve los espacios en blanco de una cadena.
+        /// </summary>
+        /// <param name="str">La cadena a procesar.</param>
+        /// <returns>La cadena sin espacios en blanco.</returns>
+        private static string RemoverEspaciosEnBlanco(this string str)
         {
-            if (str is null) throw new ArgumentNullException(nameof(str));
+            // Utiliza el método Split para dividir la cadena por espacios en blanco y luego junta los resultados
+            return string.Join(string.Empty, str.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+        }
 
+        /// <summary>
+        /// Invierte el orden de los caracteres en una cadena.
+        /// </summary>
+        /// <param name="str">La cadena a invertir.</param>
+        /// <returns>La cadena invertida.</returns>
+        private static string InvertirCaracteres(this string str)
+        {
+            // Verifica si la cadena es nula y lanza una excepción si es así
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            // Invierte los caracteres en la cadena
             string strInvertido = string.Empty;
 
             foreach (char caracter in str)
             {
-                strInvertido = caracter + strInvertido; 
+                strInvertido = caracter + strInvertido;
             }
             return strInvertido;
         }
