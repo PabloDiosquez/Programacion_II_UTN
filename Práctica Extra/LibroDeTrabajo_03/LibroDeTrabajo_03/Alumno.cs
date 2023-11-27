e2d6f7865e902fb6479e9d08b89a77b8cc5a9c1a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-27 14:17:48 -0300

    Libro de Trabajo 03

---
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Alumno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroDeTrabajo_03
+{
+    public class Alumno
+    {
+        // Atributos 
+
+        private int dni;
+
+        private string nombreEscuela;
+
+        // Constructores 
+
+        public Alumno() 
+        { }
+
+        public Alumno(int dni, string nombreEscuela)
+        {
+            this.dni = dni;
+
+            this.nombreEscuela = nombreEscuela;
+        }
+
+        // Propiedades 
+        public int Dni { get => dni; set => dni = value; }
+        public string NombreEscuela { get => nombreEscuela; set => nombreEscuela = value; }
+
+        // Sobrecarga del método ToString()
+
+        public override string ToString()
+        {
+            return $"[DNI: {this.Dni}, Escuela: {this.NombreEscuela}]";
+        }
+    }
+}
--- Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Program.cs
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Program.cs
@@ -24,5 +24,17 @@ namespace LibroDeTrabajo_03
             }
             return palindromos;
         }
+
+        // Ejercicio 2
+        //Desarrolle un programa de aplicación que simule la inscripción de participantes de distintas escuelas a una
+        //competencia de matemática.De cada alumno se conoce su Dni y el nombre de la escuela a la que representa.
+        //Pueden inscribirse(en forma desordenada) varios alumnos de la misma escuela.Arme un menú que permita:
+        //a.Inscribir un alumno (armar una lista de alumnos inscriptos)
+        //b.Borrar un alumno(eliminarlo de la lista de alumnos inscriptos)
+        //c.Indicar total de alumnos inscriptos
+        //d.Indicar total de escuelas que al menos tienen un alumno inscripto (armar una lista de escuelas)
+        //e.Imprimir el listado de las escuelas, ordenado alfabéticamente, sin repeticiones.
+
+
     }
 }
