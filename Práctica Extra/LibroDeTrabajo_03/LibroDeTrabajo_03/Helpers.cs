ecc87e190ee7769b98db12eca9d00b9967edffcd
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-27 12:04:13 -0300

    Libro de Trabajo 03

---
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Helpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroDeTrabajo_03
+{
+    public static class Helpers
+    {
+        public static bool EsPalindromo(string str) 
+        {
+            return string.Join(string.Empty, str.Split(' ', StringSplitOptions.RemoveEmptyEntries)) == InvertirTexto(str);
+        }
+
+        private static string InvertirTexto(string str)
+        {
+            string[] palabras = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            return InvertirCaracteres(string.Join(string.Empty, palabras));
+        }
+
+        private static string InvertirCaracteres(string str) 
+        {
+            if (str is null) throw new ArgumentNullException(nameof(str));
+
+            string strInvertido = string.Empty;
+
+            foreach (char caracter in str)
+            {
+                strInvertido = caracter + strInvertido; 
+            }
+            return strInvertido;
+        }
+    }
+}
---
+++ Práctica Extra/LibroDeTrabajo_03/LibroDeTrabajo_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibroDeTrabajo_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Helpers.EsPalindromo("anita lava la tina"));
+        }
+
+        // Ejercicio 1.
+        // Escriba un programa de aplicación que reciba una lista de palabras e informe cuáles son palíndromos usando una 
+        // función.
+
+        public static List<string> FiltrarPalindromos(List<string> palabras) 
+        {
+            List<string> palindromos = new List<string>();
+
+            foreach (string palabra in palabras) 
+            {
+                if(palabra.EsPalindromo()) palabras.Add(palabra);   
+            }
+            return palindromos;
+        }
+    }
+}
