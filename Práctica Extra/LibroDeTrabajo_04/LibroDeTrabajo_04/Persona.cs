bedb43b73d3a4a70f25ff9bbf453d6fbd26ef503
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-11-28 18:23:52 -0300

    Libro de Trabajo 04

---
+++ Práctica Extra/LibroDeTrabajo_04/LibroDeTrabajo_04/Persona.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroDeTrabajo_04
+{
+    /// <summary>
+    /// Representa a una persona con nombre, edad y DNI.
+    /// </summary>
+    public class Persona
+    {
+        // Campos o atributos 
+
+        /// <summary>
+        /// Almacena el nombre de la persona.
+        /// </summary>
+        private string nombre;
+
+        /// <summary>
+        /// Almacena la edad de la persona.
+        /// </summary>
+        private int edad;
+
+        /// <summary>
+        /// Almacena el DNI (Documento Nacional de Identidad) de la persona.
+        /// </summary>
+        private int dni;
+
+        // Constructores
+
+        /// <summary>
+        /// Constructor predeterminado de la clase Persona.
+        /// </summary>
+        public Persona() { }
+
+        /// <summary>
+        /// Constructor que inicializa una instancia de la clase Persona con nombre, edad y DNI.
+        /// </summary>
+        /// <param name="nombre">El nombre de la persona.</param>
+        /// <param name="edad">La edad de la persona.</param>
+        /// <param name="dni">El DNI (Documento Nacional de Identidad) de la persona.</param>
+        public Persona(string nombre, int edad, int dni)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+        }
+
+        // Propiedades 
+
+        /// <summary>
+        /// Obtiene o establece el nombre de la persona.
+        /// </summary>
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        /// <summary>
+        /// Obtiene o establece la edad de la persona.
+        /// </summary>
+        public int Edad { get => edad; set => edad = value; }
+
+        /// <summary>
+        /// Obtiene o establece el DNI (Documento Nacional de Identidad) de la persona.
+        /// </summary>
+        public int Dni { get => dni; set => dni = value; }
+
+        // Métodos de instancia 
+
+        /// <summary>
+        /// Compara la edad de la persona actual con otra persona.
+        /// </summary>
+        /// <param name="persona">La otra persona a comparar.</param>
+        /// <returns>True si la persona actual es mayor en edad que la otra persona; de lo contrario, False.</returns>
+        public bool EsMayorQue(Persona persona)
+        {
+            return this.Edad > persona.Edad;
+        }
+
+        /// <summary>
+        /// Compara la edad de la persona actual con otra persona.
+        /// </summary>
+        /// <param name="persona">La otra persona a comparar.</param>
+        /// <returns>True si la persona actual es menor en edad que la otra persona; de lo contrario, False.</returns>
+        public bool EsMenorQue(Persona persona)
+        {
+            return this.Edad < persona.Edad;
+        }
+
+        // Sobrecarga del método ToString()
+
+        /// <summary>
+        /// Devuelve una cadena que representa la información de la persona en el formato "Nombre (Edad) DNI".
+        /// </summary>
+        /// <returns>Una cadena que representa la información de la persona.</returns>
+        public override string ToString()
+        {
+            return $"{this.Nombre} ({this.Edad}) {this.Dni}";
+        }
+
+        // Sobrecarga de operadores 
+
+        /// <summary>
+        /// Compara dos instancias de Persona para determinar si son iguales.
+        /// </summary>
+        /// <param name="primerPersona">La primera persona a comparar.</param>
+        /// <param name="segundaPersona">La segunda persona a comparar.</param>
+        /// <returns>True si las personas son iguales; de lo contrario, False.</returns>
+        public static bool operator ==(Persona primerPersona, Persona segundaPersona)
+        {
+            if (primerPersona is null || segundaPersona is null) return false;
+            return primerPersona.Dni == segundaPersona.Dni && primerPersona.Nombre == segundaPersona.Nombre;
+        }
+
+        /// <summary>
+        /// Compara dos instancias de Persona para determinar si son diferentes.
+        /// </summary>
+        /// <param name="primerPersona">La primera persona a comparar.</param>
+        /// <param name="segundaPersona">La segunda persona a comparar.</param>
+        /// <returns>True si las personas son diferentes; de lo contrario, False.</returns>
+        public static bool operator !=(Persona primerPersona, Persona segundaPersona)
+        {
+            return !(primerPersona == segundaPersona);
+        }
+
+        /// <summary>
+        /// Compara dos instancias de Persona para determinar si la primera es mayor en edad que la segunda.
+        /// </summary>
+        /// <param name="primerPersona">La primera persona a comparar.</param>
+        /// <param name="segundaPersona">La segunda persona a comparar.</param>
+        /// <returns>True si la primera persona es mayor en edad que la segunda; de lo contrario, False.</returns>
+        public static bool operator >(Persona primerPersona, Persona segundaPersona)
+        {
+            return primerPersona.EsMayorQue(segundaPersona);
+        }
+
+        /// <summary>
+        /// Compara dos instancias de Persona para determinar si la primera es menor en edad que la segunda.
+        /// </summary>
+        /// <param name="primerPersona">La primera persona a comparar.</param>
+        /// <param name="segundaPersona">La segunda persona a comparar.</param>
+        /// <returns>True si la primera persona es menor en edad que la segunda; de lo contrario, False.</returns>
+        public static bool operator <(Persona primerPersona, Persona segundaPersona)
+        {
+            return primerPersona.EsMenorQue(segundaPersona);
+        }
+    }
+}
--- Práctica Extra/LibroDeTrabajo_04/LibroDeTrabajo_04/Program.cs
+++ Práctica Extra/LibroDeTrabajo_04/LibroDeTrabajo_04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LibroDeTrabajo_04
 {
@@ -6,11 +7,72 @@ namespace LibroDeTrabajo_04
     {
         static void Main(string[] args)
         {
-            Hora h1 = new Hora(23, 30, 15);
-            Hora h2 = new Hora("14:25:47");
-            h1.imprimir();
-            h2.imprimir();
-            Console.ReadKey(true);
+            // Ejercicio 1
+
+            //Hora h1 = new Hora(23, 30, 15);
+            //Hora h2 = new Hora("14:25:47");
+            //h1.Imprimir();
+            //h2.Imprimir();
+            //Console.ReadKey(true);
+
+            // Ejercicio 2
+
+            Persona rapero1 = new Persona("Tupac Shakur", 25, 98765432);
+            Persona rapero2 = new Persona("The Notorious B.I.G.", 24, 87654321);
+            Persona rapero3 = new Persona("Queen Latifah", 30, 76543210);
+
+            List<Persona> raperos = new List<Persona>() { rapero1, rapero2, rapero3};
+
+            // Ordenar y recorrer la lista de raperos 
+
+            Console.WriteLine($"Rapero de mayor edad: {PersonaMayor(raperos)}");
+
+        }
+
+        // Ejercicio 2
+
+        /// <summary>
+        /// Encuentra y devuelve la persona de mayor edad en una lista de personas.
+        /// </summary>
+        /// <param name="personas">Lista de personas entre las cuales se buscará la de mayor edad.</param>
+        /// <returns>La persona de mayor edad o null si la lista está vacía o si ocurre un error.</returns>
+        private static Persona PersonaMayor(List<Persona> personas)
+        {
+            try
+            {
+                // Verificar si la lista de personas no es nula ni vacía
+                if (personas is null || personas.Count == 0)
+                {
+                    Console.WriteLine("La lista dada no debe ser vacía.");
+                    return null;
+                }
+
+                // Inicializar la persona mayor como la primera persona en la lista
+                Persona personaMayor = personas[0];
+
+                // Iterar a través de la lista para encontrar la persona de mayor edad
+                foreach (Persona persona in personas)
+                {
+                    if (persona.EsMayorQue(personaMayor))
+                    {
+                        personaMayor = persona;
+                    }
+                }
+
+                // Devolver la persona de mayor edad encontrada
+                return personaMayor;
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("La lista dada no debe ser vacía.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Algo salió mal... \n{ex.Message}");
+            }
+
+            // Devolver null si hay algún error
+            return null;
         }
     }
 }
