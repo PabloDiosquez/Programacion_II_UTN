34218196d74aed4893482e4fb89ff560da0e0fa9
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-03 18:25:17 -0300

    Libro de Trabajo 06

---
+++ Práctica Extra/LibroDeTrabajo_06/Clases/ClaseC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class ClaseC : ClaseB
+    {
+        public override string ToString()
+        {
+            return $"Clase C {base.ToString()}";
+        }
+    }
+}
---
+++ Práctica Extra/LibroDeTrabajo_06/LibroDeTrabajo_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Clases;
+
+namespace LibroDeTrabajo_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseD objeto = new ClaseD();
+
+            Console.WriteLine(objeto);
+        }
+    }
+}
