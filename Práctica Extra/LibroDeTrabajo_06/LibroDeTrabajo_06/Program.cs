cc65040c2c494e192193ce6903111745bcf59af6
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-04 17:18:33 -0300

    Libro de Trabajo 06

--- Práctica Extra/LibroDeTrabajo_06/LibroDeTrabajo_06/Program.cs
+++ Práctica Extra/LibroDeTrabajo_06/LibroDeTrabajo_06/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Clases;
+using Espectaculo;
 
 namespace LibroDeTrabajo_06
 {
@@ -7,15 +7,55 @@ namespace LibroDeTrabajo_06
     {
         static void Main(string[] args)
         {
-            Alumno alumno = new Alumno(2998745, "Juan", "Perez", new DateTime(1997, 12, 3), 7.8);
+            Console.Write("Ingrese el precio de la obra de teatro 'El fantasma de la ópera': ");
+            double precioObra = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(alumno);
+            ObraDeTeatro elFantasmaDeLaOpera = new ObraDeTeatro("El fantasma de la ópera", 10); // 10 butacas disponibles
 
-            Alumno alumno1 = IngresarAlumno();
+            int totalEntradasSolicitadas = 0;
+            int totalEntradasVendidas = 0;
 
-            Console.WriteLine(alumno1);
+            while (true)
+            {
+                Console.Write("Ingrese el DNI del espectador (o '0' para salir): ");
+                int dni = Convert.ToInt32(Console.ReadLine());
+
+                if (dni == 0)
+                {
+                    break; // Salir del bucle si se ingresa 0
+                }
+
+                Console.Write("Ingrese el nombre del espectador: ");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Ingrese el apellido del espectador: ");
+                string apellido = Console.ReadLine();
+
+                Espectador espectador = new Espectador(dni, nombre, apellido);
+
+                if (elFantasmaDeLaOpera.VenderEntrada(espectador, precioObra))
+                {
+                    Console.WriteLine($"Entrada vendida. Nro de butaca: {espectador.NroButaca}");
+                    totalEntradasVendidas++;
+                }
+                else
+                {
+                    Console.WriteLine("No hay butacas disponibles.");
+                }
+
+                totalEntradasSolicitadas++;
+            }
+
+            Console.WriteLine("\nResumen de la venta de entradas:");
+            Console.WriteLine($"Total de entradas solicitadas: {totalEntradasSolicitadas}");
+            Console.WriteLine($"Total de entradas vendidas: {totalEntradasVendidas}");
+            Console.WriteLine($"Recaudación final: {elFantasmaDeLaOpera.totalRecaudado()}");
+
+            Console.ReadLine(); // Para que la consola no se cierre inmediatamente
         }
+    
 
+        // Ejercicio 1.
         public static Alumno IngresarAlumno() 
         {
             Console.WriteLine("Ingresar datos alumno: [DNI<TAB>Nombre<TAB>Apellido<TAB><ENTER>]");
