21c748bb586b22038ec5aa00793784df0b9acb56
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-05 16:00:24 -0300

    Libro de Trabajo 06

--- Práctica Extra/LibroDeTrabajo_06/Biblioteca_Cine/Persona.cs
+++ Práctica Extra/LibroDeTrabajo_06/Biblioteca_Cine/Persona.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Biblioteca_Cine
 {
-    internal class Persona
+    public abstract class Persona
     {
+        // Atributos 
+
+        private string nombre;
+
+        private string apellido;
+
+        private string direccion;
+
+        private string telefono;
+
+        private short edad;
+
+        // Constructores 
+
+        protected Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+
+            this.apellido = apellido;
+        }
+
+        protected Persona(string nombre, string apellido, short edad) 
+            : this(nombre, apellido)
+        {
+            this.edad = edad;
+        }
+
+        protected Persona(Persona otraPersona)
+            : this(otraPersona.nombre, otraPersona.apellido, otraPersona.edad)
+        { }
+
+        protected Persona(string nombre, string apellido, string direccion, string telefono, short edad)
+            : this(nombre, apellido, edad)
+        {
+            this.direccion = direccion;
+
+            this.telefono = telefono;
+        }
+
+        // Propiedades 
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public short Edad { get => edad; set => edad = value; }
+
+        // Sobrescritura: método ToString()
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre completo: {this.Nombre}, {this.Apellido}");
+            sb.AppendLine($"Edad: {this.Edad}");
+            sb.AppendLine($"Teléfono: {this.Telefono}");
+            sb.AppendLine($"Dirección: {this.Direccion}");
+
+            return sb.ToString();
+        }
     }
 }
