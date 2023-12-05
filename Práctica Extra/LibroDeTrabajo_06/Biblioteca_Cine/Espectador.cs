441c47cfe450b0e6b4304d512dd9f988e7d288dc
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-05 15:25:53 -0300

    Libro de Trabajo 06

--- Práctica Extra/LibroDeTrabajo_06/Biblioteca_Cine/Espectador.cs
+++ Práctica Extra/LibroDeTrabajo_06/Biblioteca_Cine/Espectador.cs
@@ -4,9 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public enum MetodoPago 
+{
+    Efectivo,
+    Tarjeta,
+    Cheque,
+}
+
 namespace Biblioteca_Cine
 {
-    internal class Espectador
+    public class Espectador : Persona
     {
+        // Atributos 
+
+        private MetodoPago metodoPago;
+
+        // Constructores 
+
+        public Espectador(string nombre, string apellido, MetodoPago metodoPago)
+            : base(nombre, apellido)
+        {
+            this.metodoPago = metodoPago;
+        }
+
+        public Espectador(Espectador espectador)
+            : this(espectador.Nombre, espectador.Apellido, espectador.MetodoPago)
+        { }
+
+        // Propiedades 
+
+        public MetodoPago MetodoPago { get => this.metodoPago; set => this.metodoPago = value; }
+
+        // Sobrescritura: método ToString()
+
+        public override string ToString()
+        {
+            return $"[Nombre: {this.Nombre}, Apellido: {this.Apellido}, Método de pago: {this.MetodoPago}]";
+        }
     }
 }
