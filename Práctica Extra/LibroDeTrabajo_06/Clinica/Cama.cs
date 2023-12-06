0acb1173aebb849392a758b41f22d896940371b9
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-06 17:42:24 -0300

    Libro de Trabajo 06

--- Práctica Extra/LibroDeTrabajo_06/Clinica/Cama.cs
+++ Práctica Extra/LibroDeTrabajo_06/Clinica/Cama.cs
@@ -8,5 +8,35 @@ namespace Clinica
 {
     public class Cama
     {
+        // Atributos 
+
+        private int nroCama;
+
+        private string nombrePaciente;
+
+        private string diagnostico;
+
+        private ObraSocial obraSocial;
+
+        private DateTime fechaIngreso;
+
+        public Cama(int nroCama)
+        {
+            this.nroCama = nroCama;
+        }
+
+        // Propiedades 
+        public int NroCama { get => nroCama; set => nroCama = value; }
+        public string NombrePaciente { get => nombrePaciente; set => nombrePaciente = value; }
+        public string Diagnostico { get => diagnostico; set => diagnostico = value; }
+        public ObraSocial ObraSocial { get => obraSocial; set => obraSocial = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+
+        // Métodos 
+
+        public override string ToString()
+        {
+            return $"[Nro de cama: {this.NroCama}]";
+        }
     }
 }
--- Práctica Extra/LibroDeTrabajo_06/Clinica/ObraSocial.cs
+++ Práctica Extra/LibroDeTrabajo_06/Clinica/ObraSocial.cs
@@ -8,5 +8,18 @@ namespace Clinica
 {
     public class ObraSocial
     {
+        private string nombre;
+
+        private string cobertura;
+
+        public ObraSocial(string nombre, string cobertura)
+        {
+            this.nombre = nombre;
+
+            this.cobertura = cobertura;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Cobertura { get => cobertura; set => cobertura = value; }
     }
 }
